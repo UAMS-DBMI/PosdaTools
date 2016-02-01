03802ba54e695ccc571f3f4f5fb6224350acf72e
Quasar Jarosz <jjarosz@uams.edu>
2016-02-01 16:47:12 -0600

    Simplified Posda::Log, removed use of closures.

--- Posda/include/Posda/Log.pm
+++ Posda/include/Posda/Log.pm
@@ -1,55 +1,34 @@
 package Posda::Log;
 # This is a very simple logging library.
-# Usage is simple:
 #
-# use Posda::Log;  # ensure this comes AFTER your package declaration!
-#
-# # Or, if you want to use a custom name for the logger:
-# use Posda::Log "logger_name";
-#
-# Logging functions will then be imported as below:
 # These levels are supported, Low to High:
-#
-# DEBUG
-# INFO
-# WARN
-# ERROR
-# FATAL
-#
-# If given,  logger_name will be printed instead of the calling module.
+# 0 DEBUG
+# 1 INFO
+# 2 WARN
+# 3 ERROR
+# 4 FATAL
 
+require Exporter;
+
+@ISA = qw(Exporter);
+@EXPORT = qw(DEBUG INFO WARN ERROR FATAL);
 my $configured;
 
-sub import {
-  no strict 'refs';
-
-  my ($this, $logger_name) = @_;
-
-  # If a logger name is not used, just use the caller's name
-  # This is probably what the user wants 99% of the time anyway.
-  if (not defined ($logger_name)) {
-    $logger_name = caller;
-  }
-
-  my $caller = caller;
-
-  # Manually build a set of closures in the caller's namespace
-  *{ "${caller}::DEBUG" } = sub {
-    dispatch_message(0, "$logger_name: @_");
-  };
-  *{ "${caller}::INFO" } = sub {
-    dispatch_message(1, "$logger_name: @_");
-  };
-  *{ "${caller}::WARN" } = sub {
-    dispatch_message(2, "$logger_name: @_");
-  };
-  *{ "${caller}::ERROR" } = sub {
-    dispatch_message(3, "$logger_name: @_");
-  };
-  *{ "${caller}::FATAL" } = sub {
-    dispatch_message(4, "$logger_name: @_");
-  };
+sub DEBUG {#{{{
+  dispatch_message(0, scalar caller, @_);
 }
+sub INFO {
+  dispatch_message(1, scalar caller, @_);
+}
+sub WARN {
+  dispatch_message(2, scalar caller, @_);
+}
+sub ERROR {
+  dispatch_message(3, scalar caller, @_);
+}
+sub FATAL {
+  dispatch_message(4, scalar caller, @_);
+}#}}}
 
 
 { # inline package def
@@ -81,6 +60,7 @@ sub import {
     }
   }
 };
+
 
 our @names = qw(DEBUG INFO WARN ERROR FATAL);
 our @outputs = ();
@@ -119,10 +99,12 @@ sub init_screen_only {
 }
 
 sub dispatch_message {
-  my $level = shift @_;
+  $level = shift @_;
+  $caller = shift @_;
+
   if (not defined($configured)){
     init_screen_only();
-    dispatch_message(2, "Explicit Posda::Log::init not called, defaulting to screen-only output!");
+    dispatch_message(2, $caller, "Explicit Posda::Log::init not called, defaulting to screen-only output!");
   }
 
   # apply rules based on current setting
@@ -130,7 +112,7 @@ sub dispatch_message {
 
   # Simply print to every output
   foreach my $output (@outputs) {
-    $output->print($level, "$level_name: @_\n");
+    $output->print($level, "$level_name: $caller: @_\n");
   }
 }
 
