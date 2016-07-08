f33539ccc80b3d38130e6c3f22b39bd3378995b6
Quasar Jarosz <jjarosz@uams.edu>
2016-07-08 16:00:06 -0500

    Added script to install on Ubuntu 16.04 LTS

---
+++ install_files/ubuntu_1604_everything.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get install -y postgresql-9.5 postgresql-server-dev-9.5 postgresql-client-9.5
+sudo apt-get install -y libmodern-perl-perl libmethod-signatures-simple-perl libdbd-pg-perl libjson-perl libswitch-perl libdata-uuid-perl libtext-diff-perl libterm-readkey-perl
+
+echo "=========================================="
+echo "=========================================="
+echo "=========================================="
+echo
+
+echo "Adding the current user as a postgres superuser"
+sudo -u postgres createuser -s $(whoami)
+
+./create_cache_dirs.sh
+./create_databases.sh
+
+echo "Everything should be ready to go now!"
