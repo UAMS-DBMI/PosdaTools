0e1c6c5173372030c389a78e5707d539d6ef1f8d
Quasar Jarosz <jjarosz@uams.edu>
2016-07-11 11:19:37 -0500

    Adjust config file name/location

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-. Config/main.env
+if [ ! -e posda.env ]; then
+  echo "Missing posda.env! You must copy posda.env.example to posda.env and edit it first!";
+  exit 1;
+fi
+
+. posda.env
 perl bin/setup.pl
