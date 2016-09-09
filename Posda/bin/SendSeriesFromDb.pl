27ad8cbee8dccde80c719ea62098daeb9d24e491
Bill Bennett <wcbennett@uams.edu>
2016-09-09 13:54:29 -0500

    Adding SendSeriesFromDb.pl

---
+++ Posda/bin/SendSeriesFromDb.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/perl -w
+#
+#Copyright 2013, Bill Bennett
+# Part of the Posda package
+# Posda may be copied only under the terms of either the Artistic License or the
+# GNU General Public License, which may be found in the Posda Distribution,
+# or at http://posda.com/License.html
+#
+use strict;
+use DBI;
+use Posda::DB::PosdaFilesQueries;
+use Socket;
+use Storable qw( store retrieve retrieve fd_retrieve store_fd );
+use Debug;
+my $dbg = sub { print STDERR @_ };
+my $help = <<EOF;
+Usage: 
+  DicomSendSeriesFromDb.pl <host> <port> <called> <calling> <series> <num_simul>
+or
+  DicomSendSeriesFromDb.pl -h
+EOF
+
+if($#ARGV < 5 || ($ARGV[0] eq "-h")){
+  print $help;
+  exit;
+}
+my $q_inst = PosdaDB::Queries->GetQueryInstance("FilesInSeriesForSend");
+my $db_name = $q_inst->GetSchema;
+my $dbh = DBI->connect("dbi:Pg:dbname=$db_name");
+unless($dbh) { die "Can't connect to $db_name" }
+my %Hash;
+$Hash{host} = shift @ARGV;
+$Hash{port} = shift @ARGV;
+$Hash{called} = shift @ARGV;
+$Hash{calling} = shift @ARGV;
+my $series_instance_uid = shift @ARGV;
+$Hash{num_simul} = shift @ARGV;
+$Hash{FilesToSend} = [];
+$Hash{FilesByDigest} = {};
+$Hash{FilesToDigest} = {};
+my $add_file = sub {
+  my($row) = @_;
+  $Hash{FilesToDigest}->{$row->{path}} = $row->{digest};
+  push @{$Hash{FilesToSend}}, $row->{path};
+  my $file_desc = {
+    dataset_start_offset => $row->{data_set_start},
+    dataset_size => $row->{data_set_size},
+    xfr_stx => $row->{xfer_syntax},
+    file => $row->{path},
+    sop_class_uid => $row->{sop_class_uid},
+    sop_inst_uid => $row->{sop_instance_uid},
+  };
+  $Hash{FilesFromDigest}->{$row->{digest}} = $file_desc;
+};
+$q_inst->Prepare($dbh);
+$q_inst->Execute($series_instance_uid);
+$q_inst->Rows($add_file);
+#print STDERR "Specification: ";
+#Debug::GenPrint($dbg, \%Hash, 1);
+#print STDERR "\n";
+my $cmd = "SendFileListOneFilePerAssoc.pl \"$Hash{host}\" " .
+  "\"$Hash{port}\" \"$Hash{called}\" \"$Hash{calling}\" " .
+  "\"$Hash{num_simul}\"";
+open FILE, "|$cmd" or die "can't open pipe to $cmd";
+for my $file (@{$Hash{FilesToSend}}){
+  print FILE "$file\n";
+#  print "$file\n";
+}
