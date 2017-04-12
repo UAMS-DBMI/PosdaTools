7875f71e7e36bdd6e46b05319d4ff3e208f31dbe
Quasar Jarosz <jjarosz@uams.edu>
2017-04-12 15:28:05 -0500

    Removed unusued page-not-found-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,26 +7,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { Component1Component } from './component1/component1.component';
 import { SeriesComponentComponent } from './series-component/series-component.component';
-import { PageNotFoundComponentComponent } from './page-not-found-component/page-not-found-component.component';
-
 import { SeriesService } from './series.service';
 import { ErrorService, ErrorComponent } from './errors';
 import { NavComponent } from './nav/nav.component';
-
-
-// const appRoutes: Routes = [
-//  { path: 'iec/:id', component: SeriesComponentComponent },
-//  { path: '', component: Component1Component },
-//  { path: '**', component: PageNotFoundComponentComponent },
-// ];
-
 
 @NgModule({
   declarations: [
     AppComponent,
     Component1Component,
     SeriesComponentComponent,
-  PageNotFoundComponentComponent,
   ErrorComponent,
   NavComponent
   ],
