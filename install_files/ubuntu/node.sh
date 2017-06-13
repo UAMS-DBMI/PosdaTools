d25b3d6abb67cba94ddd444b4882f1858ffce30f
Quasar Jarosz <jjarosz@uams.edu>
2017-06-13 11:07:44 -0500

    Fully pin node version so service file works

--- install_files/ubuntu/node.sh
+++ install_files/ubuntu/node.sh
@@ -5,6 +5,6 @@ wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
-nvm install v8
+nvm install v8.1.0
 
 ./node_tools.sh
