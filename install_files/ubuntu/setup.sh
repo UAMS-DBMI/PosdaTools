dbb27cfedaddc47450eb250cf878cf3d58e0a334
Quasar Jarosz <jjarosz@uams.edu>
2017-06-13 11:05:35 -0500

    Add libjpeg-dev, for node-canvas

--- install_files/ubuntu/setup.sh
+++ install_files/ubuntu/setup.sh
@@ -16,7 +16,7 @@ CPAN="sudo cpanm --notest"
 
 sudo apt-get update
 # need python2 for building k-base later
-$APT build-essential zlib1g-dev libssl-dev python libcairo2-dev
+$APT build-essential zlib1g-dev libssl-dev python libcairo2-dev libjpeg-dev
 $APT postgresql-9.5 postgresql-server-dev-9.5 postgresql-client-9.5 nginx
 $APT libmodern-perl-perl libmethod-signatures-simple-perl libdbd-pg-perl libjson-perl libswitch-perl libdata-uuid-perl libtext-diff-perl libterm-readkey-perl cpanminus libdatetime-perl libnet-ldap-perl
 
