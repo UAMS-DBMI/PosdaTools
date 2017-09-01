7773091bd25d730d3383f65dc7e0180b3f287118
Quasar Jarosz <quasar@uams.edu>
2017-09-01 16:29:40 -0500

    Fix bug with sendmail path

--- python/posda/background/email.py
+++ python/posda/background/email.py
@@ -2,7 +2,7 @@ from email.mime.text import MIMEText
 from subprocess import Popen, PIPE
 
 def send_email(address, subject, content):
-    p = Popen(["/usr/bin/sendmail", "-i", "--", address], stdin=PIPE)
+    p = Popen(["/usr/sbin/sendmail", "-i", "--", address], stdin=PIPE)
     msg = MIMEText(content)
     msg['Subject'] = subject
     msg['To'] = address
