59f02744973676bf9cad186eb7c4508a83b13498
Quasar Jarosz <quasar@uams.edu>
2017-11-08 14:18:15 -0600

    Add simple install test

---
+++ tests/install_test.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Execute some high-level tests to see if the install worked
+failed=0
+
+# units
+systemctl is-active posda                 || failed=1
+systemctl is-active posda-backlog         || failed=1
+systemctl is-active posda-file-process    || failed=1
+
+# databases
+psql -l | grep -q dicom_dd                || failed=1
+psql -l | grep -q dicom_roots             || failed=1
+psql -l | grep -q posda_appstats          || failed=1
+psql -l | grep -q posda_auth              || failed=1
+psql -l | grep -q posda_backlog           || failed=1
+psql -l | grep -q posda_counts            || failed=1
+psql -l | grep -q posda_files             || failed=1
+psql -l | grep -q posda_nicknames         || failed=1
+psql -l | grep -q posda_phi               || failed=1
+psql -l | grep -q posda_phi_simple        || failed=1
+psql -l | grep -q posda_queries           || failed=1
+psql -l | grep -q private_tag_kb          || failed=1
+psql -l | grep -q public_tag_disposition  || failed=1
+
+exit $failed
