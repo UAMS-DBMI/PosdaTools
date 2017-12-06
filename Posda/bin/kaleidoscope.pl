c2fa6819aa2f8f2bcef18b3f123ccae7bbd0b42b
Quasar Jarosz <quasar@uams.edu>
2017-12-06 16:29:38 -0600

    Adjust kaleidoscope.pl to use POSDA_EXTERNAL_HOSTNAME
    This resolves PT-465.

--- Posda/bin/kaleidoscope.pl
+++ Posda/bin/kaleidoscope.pl
@@ -2,6 +2,9 @@
 
 use JSON;
 use REST::Client;
+use Try::Tiny;
+
+my $external_url = $ENV{POSDA_EXTERNAL_HOSTNAME};
 
 $|++;
 
@@ -11,13 +14,18 @@ my $dir = $ARGV[2];
 my $color = $ARGV[3];
 my $user = $ARGV[4];
 
+try {
+  my $client = REST::Client->new();
+  # Currently, assume the webservice is running on localhost
+  # TODO: this should probably be a configuration variable!
+  $client->GET("http://localhost/api/new_token/$user");
+  my $resp = decode_json($client->responseContent());
+  my $token = $resp->{token};
 
-my $client = REST::Client->new();
-$client->GET("http://localhost/api/new_token/$user");
-my $resp = decode_json($client->responseContent());
-my $token = $resp->{token};
 
-
-print "Redirect to http://tcia-posda-rh-1.ad.uams.edu/k/?token=$token\n";
+  print "Redirect to http://$external_url/k/?token=$token\n";
+} catch {
+  print "Redirect to http://$external_url/k/error.html\n";
+};
 
 sleep 20;
