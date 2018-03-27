7a3f74844bf6f9415bfd4254bf4d5d5a33cb26dd
Quasar Jarosz <quasar@uams.edu>
2018-03-27 17:30:11 -0500

    Add Docker-friendly hostname for Kaleidoscope location
    This will break non-Docker installs, so this likely should
    be addressed; maybe moved to an environment variable?

--- Posda/bin/kaleidoscope.pl
+++ Posda/bin/kaleidoscope.pl
@@ -18,13 +18,14 @@ try {
   my $client = REST::Client->new();
   # Currently, assume the webservice is running on localhost
   # TODO: this should probably be a configuration variable!
-  $client->GET("http://localhost/api/new_token/$user");
+  $client->GET("http://kaleidoscope:8089/api/new_token/$user");
   my $resp = decode_json($client->responseContent());
   my $token = $resp->{token};
 
 
   print "Redirect to http://$external_url/k/?token=$token\n";
 } catch {
+  print "Login token error: $@\n";
   print "Redirect to http://$external_url/k/error.html\n";
 };
 
