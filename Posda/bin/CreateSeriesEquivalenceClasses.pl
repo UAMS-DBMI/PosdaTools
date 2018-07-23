da9f5bb2bebe0e4c1d73dd2cb847ddd583240ef0
Sonya Utecht <sutecht@uams.edu>
2018-07-23 14:07:00 -0500

    Initial commit to new branch.

--- Posda/bin/CreateSeriesEquivalenceClasses.pl
+++ Posda/bin/CreateSeriesEquivalenceClasses.pl
@@ -2,6 +2,7 @@
 use strict;
 use Posda::DB::PosdaFilesQueries;
 use Debug;
+use Data::Dumper;
 my $dbg = sub {print STDERR @_};
 my $usage = <<EOF;
 CreateSeriesEquivalenceClasses.pl <series_instance_uid>
@@ -34,34 +35,57 @@ my @col_headers = (
   "pixel_rows",
   "pixel_columns",
   "file_id",
+  "ipp"
 );
-my %Separator;
+#creates a large tree made of hashes. These hashes are "walked through" in the later steps
+my %files;
 $q_inst->RunQuery(
   sub {
     my($row) = @_;
     my @cols = @$row;
-    my $h_ptr = \%Separator;
     my $i;
-    for $i (0 .. $#col_headers - 1){
+    my $debugtabs= 0; #debug tabs added for printing debug output
+    #loops through all the fields
+    for $i (0 .. $#col_headers){
+      #if the field is empty set it to undefined
       unless(defined $cols[$i]) { $cols[$i] = "<undef>" }
-      unless(exists($h_ptr->{$cols[$i]})) {
-        $h_ptr->{$cols[$i]} = {};
-      }
-      $h_ptr = $h_ptr->{$cols[$i]};
+      #if there is not a matching key...
+      # unless(exists($h_ptr->{$cols[$i]})) {
+      #print "\nset to empty\n";
+      $files{$col_headers[$i]} = $cols[$i];
+      #$debugtabs = 0;
+      #print "\n";
+      #}
+
+      #DEBUG PRINTING
+      # my $k = 0;
+      #for $k (0 .. $debugtabs){
+      #  print " ";
+      #}
+
+      # print "\nrecurse\n";
+      #$debugtabs++;
+      #$h_ptr = $h_ptr->{$cols[$i]};
+      #print "$i: $col_headers[$i] = $cols[$i]\n"; 
     };
-    unless(defined $cols[$#col_headers]){
-      $cols[$#col_headers] = "<undef>";
-    }
-    $h_ptr->{$cols[$#col_headers]} = 1;
+    #unless(defined $cols[$#col_headers]){
+    #  $cols[$#col_headers] = "<undef>";
+    #}
+    #$h_ptr->{$cols[$#col_headers]} = 1;
   }, 
   sub {}, 
   $ARGV[0]
 );
+
+print Dumper(\%files);
+
 my @equiv_classes;
 my $level = 0;
 my $num_levels = $#col_headers - 1;
-my $h_ptr = \%Separator;
-WalkSeparator($h_ptr, $level, $num_levels, \@equiv_classes);
+my $files_ptr = \%files;
+print "\nBegin Walk Separator\n";
+WalkSeparator($files_ptr, $level, $num_levels, \@equiv_classes);
+print "\nEnd Walk Separator\n";
 my $num_equiv = @equiv_classes;
 print "$num_equiv classes for series $ARGV[0]:\n";
 my $ins_equiv = PosdaDB::Queries->GetQueryInstance("CreateEquivalenceClass");
@@ -71,8 +95,10 @@ my $ins_equiv_file = PosdaDB::Queries->GetQueryInstance(
 my $upd_proc_stat = PosdaDB::Queries->GetQueryInstance(
   "UpdateEquivalenceClassProcessingStatus");
 for my $i (0 .. $#equiv_classes){
+  #inserts 'series_instance_uid' and 'equivalence_class_number' into Image_Equivalence_Class table with 'Preparing' status  
   $ins_equiv->RunQuery(sub{}, sub{}, $ARGV[0], $i);
   my $id = $i;
+  #gets the newly generated Sequence Id from that table
   $get_id->RunQuery(
     sub {
        my($row) = @_;
@@ -81,20 +107,27 @@ for my $i (0 .. $#equiv_classes){
     sub {}
   );
   print "[$id]-[$i]$ARGV[0]:\n"; 
+  #loop through each equivalnce class array
   for my $file_id (@{$equiv_classes[$i]}){
-     print "\t[$id] file: $file_id\n";
+    #print the array id and the file id
+    print "\t[$id] file: $file_id\n";
+    #insert into the Image_Equivalence_Class_Input_Image table the corresponding IEC table record id and the file id
     $ins_equiv_file->RunQuery(sub {}, sub {}, $id, $file_id);
   }
+  #when complete, change status of the IEC table record from 'Preparing' to 'ReadytoProcess'
   $upd_proc_stat->RunQuery(sub{}, sub{}, "ReadyToProcess", $id);
 }
 
 sub WalkSeparator{
   my($tree, $cur_level, $max_level, $equiv_classes) = @_;
   if($cur_level == $max_level){
+    #at each level create an array of files? and assign it to equivalence class array?
     my @files = keys %$tree;
+    print "\nThis level has $#files files.\n";
     push @$equiv_classes, \@files;
   } else {
     for my $k (keys %$tree){
+      #get to the bottom of the tree (h_ptr)
       WalkSeparator($tree->{$k}, $cur_level + 1, $max_level, $equiv_classes);
     }
   }
--- queries/sql/ForConstructingSeriesEquivalenceClasses.sql
+++ queries/sql/ForConstructingSeriesEquivalenceClasses.sql
@@ -1,23 +1,26 @@
 -- Name: ForConstructingSeriesEquivalenceClasses
 -- Schema: posda_files
--- Columns: ['series_instance_uid', 'modality', 'series_number', 'laterality', 'series_date', 'dicom_file_type', 'performing_phys', 'protocol_name', 'series_description', 'operators_name', 'body_part_examined', 'patient_position', 'smallest_pixel_value', 'largest_pixel_value', 'performed_procedure_step_id', 'performed_procedure_step_start_date', 'performed_procedure_step_desc', 'performed_procedure_step_comments', 'image_type', 'iop', 'pixel_rows', 'pixel_columns', 'file_id']
+-- Columns: ['series_instance_uid', 'modality', 'series_number', 'laterality', 'series_date', 'dicom_file_type', 'performing_phys', 'protocol_name', 'series_description', 'operators_name', 'body_part_examined', 'patient_position', 'smallest_pixel_value', 'largest_pixel_value', 'performed_procedure_step_id', 'performed_procedure_step_start_date', 'performed_procedure_step_desc', 'performed_procedure_step_comments', 'image_type', 'iop', 'pixel_rows', 'pixel_columns', 'file_id','ipp']
 -- Args: ['series_instance_uid']
 -- Tags: ['consistency', 'find_series', 'equivalence_classes']
 -- Description: For building series equivalence classes
 
-select distinct
-  series_instance_uid, modality, series_number, laterality, series_date, dicom_file_type,
+select distinct 
+ series_instance_uid, modality, series_number, laterality, series_date, dicom_file_type,
   performing_phys, protocol_name, series_description,
   operators_name, body_part_examined, patient_position,
   smallest_pixel_value, largest_pixel_value, performed_procedure_step_id,
   performed_procedure_step_start_date
   performed_procedure_step_desc, performed_procedure_step_comments, image_type,
   iop, pixel_rows, pixel_columns,
-  file_id
+  file_id,ipp
 from
   file_series natural join ctp_file natural join dicom_file
   left join file_image using(file_id)
   left join image using (image_id)
   left join file_image_geometry using (file_id)
   left join image_geometry using (image_geometry_id)
-where series_instance_uid = ? and visibility is null
+where series_instance_uid = '1.3.6.1.4.1.14519.5.2.1.3983.1600.225936344090001524288753349553' and visibility is null
+ 
+
+
