07748b6c9641156a992e104221ba813ba1be98ff
Sonya Utecht <sutecht@uams.edu>
2018-07-30 15:09:45 -0500

    First chunk of IEC splitting added (non geometric data equivalence)

--- Posda/bin/CreateSeriesEquivalenceClasses.pl
+++ Posda/bin/CreateSeriesEquivalenceClasses.pl
@@ -37,99 +37,133 @@ my @col_headers = (
   "file_id",
   "ipp"
 );
-#creates a large tree made of hashes. These hashes are "walked through" in the later steps
-my %files;
+
+
+#After Runquery this will be a collection of all the "files" in the series
+my $Separator= {};
+
 $q_inst->RunQuery(
+  # Do this sub for every row returned by q_inst(ForConstructingSeriesEquivalenceClasses)
   sub {
-    my($row) = @_;
-    my @cols = @$row;
-    my $i;
-    my $debugtabs= 0; #debug tabs added for printing debug output
-    #loops through all the fields
-    for $i (0 .. $#col_headers){
-      #if the field is empty set it to undefined
-      unless(defined $cols[$i]) { $cols[$i] = "<undef>" }
-      #if there is not a matching key...
-      # unless(exists($h_ptr->{$cols[$i]})) {
-      #print "\nset to empty\n";
-      $files{$col_headers[$i]} = $cols[$i];
-      #$debugtabs = 0;
-      #print "\n";
-      #}
-
-      #DEBUG PRINTING
-      # my $k = 0;
-      #for $k (0 .. $debugtabs){
-      #  print " ";
-      #}
-
-      # print "\nrecurse\n";
-      #$debugtabs++;
-      #$h_ptr = $h_ptr->{$cols[$i]};
-      #print "$i: $col_headers[$i] = $cols[$i]\n"; 
-    };
-    #unless(defined $cols[$#col_headers]){
-    #  $cols[$#col_headers] = "<undef>";
-    #}
-    #$h_ptr->{$cols[$#col_headers]} = 1;
-  }, 
+      my($row) = @_;
+      my @cols = @$row;
+      my $i;
+      my $file = {};
+        #loop through all the fields
+        for $i (0 .. $#col_headers){
+          #if the field is empty, set it to undefined
+          unless(defined $cols[$i]) { $cols[$i] = "<undef>" }
+          #set each field - value pair for the file
+          $file->{$col_headers[$i]} = $cols[$i];
+        };
+        #Add this file to the file collection
+        $Separator->{$file->{file_id}}=$file;
+      }, 
   sub {}, 
   $ARGV[0]
 );
 
-print Dumper(\%files);
+#Debug
+#print "Separator\n", Dumper($Separator);
 
-my @equiv_classes;
-my $level = 0;
-my $num_levels = $#col_headers - 1;
-my $files_ptr = \%files;
-print "\nBegin Walk Separator\n";
-WalkSeparator($files_ptr, $level, $num_levels, \@equiv_classes);
-print "\nEnd Walk Separator\n";
-my $num_equiv = @equiv_classes;
-print "$num_equiv classes for series $ARGV[0]:\n";
-my $ins_equiv = PosdaDB::Queries->GetQueryInstance("CreateEquivalenceClass");
-my $get_id = PosdaDB::Queries->GetQueryInstance("GetEquivalenceClassId");
-my $ins_equiv_file = PosdaDB::Queries->GetQueryInstance(
-  "CreateEquivalenceInputClass");
-my $upd_proc_stat = PosdaDB::Queries->GetQueryInstance(
-  "UpdateEquivalenceClassProcessingStatus");
-for my $i (0 .. $#equiv_classes){
-  #inserts 'series_instance_uid' and 'equivalence_class_number' into Image_Equivalence_Class table with 'Preparing' status  
-  $ins_equiv->RunQuery(sub{}, sub{}, $ARGV[0], $i);
-  my $id = $i;
-  #gets the newly generated Sequence Id from that table
-  $get_id->RunQuery(
-    sub {
-       my($row) = @_;
-       $id = $row->[0]
-    },
-    sub {}
-  );
-  print "[$id]-[$i]$ARGV[0]:\n"; 
-  #loop through each equivalnce class array
-  for my $file_id (@{$equiv_classes[$i]}){
-    #print the array id and the file id
-    print "\t[$id] file: $file_id\n";
-    #insert into the Image_Equivalence_Class_Input_Image table the corresponding IEC table record id and the file id
-    $ins_equiv_file->RunQuery(sub {}, sub {}, $id, $file_id);
+#check that the non-geometric fields match the template
+
+#First separate any IEC groups that differ on non geometric data
+#Each potential IEC group will have a template, and matching files will be members
+
+my $templates={};
+foreach my $file_id (keys %$Separator){
+  my $match = 0;
+  foreach my $tmpl_id (keys %$templates){
+     if(HashCompare($Separator->{$tmpl_id},$Separator->{$file_id})){
+        #add this file to this templates members
+        push @{$templates->{$tmpl_id}}, $file_id; 
+        $match = 1; 
+        last;
+      }  
   }
-  #when complete, change status of the IEC table record from 'Preparing' to 'ReadytoProcess'
-  $upd_proc_stat->RunQuery(sub{}, sub{}, "ReadyToProcess", $id);
-}
-
-sub WalkSeparator{
-  my($tree, $cur_level, $max_level, $equiv_classes) = @_;
-  if($cur_level == $max_level){
-    #at each level create an array of files? and assign it to equivalence class array?
-    my @files = keys %$tree;
-    print "\nThis level has $#files files.\n";
-    push @$equiv_classes, \@files;
-  } else {
-    for my $k (keys %$tree){
-      #get to the bottom of the tree (h_ptr)
-      WalkSeparator($tree->{$k}, $cur_level + 1, $max_level, $equiv_classes);
-    }
+  unless($match){
+    #since no templates matched, create  a new one and add this as its first member
+    push @{$templates->{$file_id}}, $file_id;
   }
 }
 
+print "\nTemplates\n";
+print Dumper(\$templates);
+
+#Within each group compare the geometric data to separate the files into the final IECs
+
+
+
+
+
+##my @equiv_classes;
+      #my $level = 0;
+#my $num_levels = $#col_headers - 1;
+#my $files_ptr = \%Separator;
+#print "\nBegin Walk Separator\n";
+#WalkSeparator($files_ptr, $level, $num_levels, \@equiv_classes);
+#print "\nEnd Walk Separator\n";
+#my $num_equiv = @equiv_classes;
+#print "$num_equiv classes for series $ARGV[0]:\n";
+#my $ins_equiv = PosdaDB::Queries->GetQueryInstance("CreateEquivalenceClass");
+#my $get_id = PosdaDB::Queries->GetQueryInstance("GetEquivalenceClassId");
+#my $ins_equiv_file = PosdaDB::Queries->GetQueryInstance(
+#  "CreateEquivalenceInputClass");
+#my $upd_proc_stat = PosdaDB::Queries->GetQueryInstance(
+#  "UpdateEquivalenceClassProcessingStatus");
+#for my $i (0 .. $#equiv_classes){
+#  #inserts 'series_instance_uid' and 'equivalence_class_number' into Image_Equivalence_Class table with 'Preparing' status  
+#  $ins_equiv->RunQuery(sub{}, sub{}, $ARGV[0], $i);
+#  my $id = $i;
+#  #gets the newly generated Sequence Id from that table
+#  $get_id->RunQuery(
+#    sub {
+#       my($row) = @_;
+#       $id = $row->[0]
+#    },
+#    sub {}
+#  );
+#  print "[$id]-[$i]$ARGV[0]:\n"; 
+#  #loop through each equivalnce class array
+#  for my $file_id (@{$equiv_classes[$i]}){
+#    #print the array id and the file id
+#    print "\t[$id] file: $file_id\n";
+#    #insert into the Image_Equivalence_Class_Input_Image table the corresponding IEC table record id and the file id
+#    $ins_equiv_file->RunQuery(sub {}, sub {}, $id, $file_id);
+#  }
+#  #when complete, change status of the IEC table record from 'Preparing' to 'ReadytoProcess'
+#  $upd_proc_stat->RunQuery(sub{}, sub{}, "ReadyToProcess", $id);
+#}
+
+#sub WalkSeparator{
+#  my($tree, $cur_level, $max_level, $equiv_classes) = @_;
+#  if($cur_level == $max_level){
+#    #at each level create an array of files? and assign it to equivalence class array?
+#    my @files = keys %$tree;
+#    print "\nThis level has $#files files.\n";
+#    push @$equiv_classes, \@files;
+#  } else {
+#    for my $k (keys %$tree){
+#      #get to the bottom of the tree (h_ptr)
+#      WalkSeparator($tree->{$k}, $cur_level + 1, $max_level, $equiv_classes);
+#    }
+#  }
+#}
+
+#Compare the string values of matching keys (ignoring geometric data)
+sub HashCompare{
+  my ($hash1,$hash2) = @_;
+      foreach my $key (keys(%$hash1)){
+          if ($key ne "iop" and $key ne "ipp" and $key ne "file_id"){ 
+             if($hash1->{$key} eq $hash2->{$key}){
+               #matches, continue the check
+             }else{ 
+                #not a match to this template, end the check
+                return 0;
+             }
+          }
+        }
+  return 1;
+}
+      
