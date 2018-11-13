21c6c8c05e03797abbb8f16bdf0027bcb3024f03
Quasar Jarosz <quasar@uams.edu>
2018-11-13 15:22:57 -0600

    Adjust all references to quasarj to tcia

--- utils/docker_psql.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-echo "Executing psql inside the database docker container..."
-
-docker exec -it posda2_db_1 psql -U postgres $@
--- utils/psql_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-echo "Executing local psql command, connecting to docker db"
-
-export PGHOST=localhost
-export PGPORT=5433
-export PGUSER=postgres
-export PGPASSWORD=example
-
-psql $@
