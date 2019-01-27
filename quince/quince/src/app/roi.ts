4f3704636d40e2284d44937d60c36398730ed718
Quasar Jarosz <quasar@uams.edu>
2019-01-27 11:36:08 -0600

    Adjust Roi interface to match API return

--- quince/quince/src/app/roi.ts
+++ quince/quince/src/app/roi.ts
@@ -1,11 +1,5 @@
 export interface Roi {
-    roi_id: number;
-    roi_name: number;
-    roi_contour_id: number;
-    pixel_rows: number;
-    pixel_columns: number;
-    ipp: number[];
-    pixel_spacing: number[];
-    roi_color: number[];
-    contour_data: number[][];
+    name: number;
+    color: number[];
+    points: number[][];
 }
