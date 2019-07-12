d9f9cf2a6b04778370899354900e5e702385968f
Quasar Jarosz <quasar@uams.edu>
2019-07-12 13:36:29 -0500

    Add subprocess_invocation_id when creating a Visual Review Instance

---
+++ posda/posdatools/queries/sql/CreateVisualReviewInstanceWithInvocId.sql
@@ -0,0 +1,16 @@
+-- Name: CreateVisualReviewInstanceWithInvocId
+-- Schema: posda_files
+-- Columns: []
+-- Args: ['invocation_id', 'visual_review_reason', 'visual_review_scheduler', 'visual_review_num_series']
+-- Tags: ['find_series', 'equivalence_classes', 'consistency', 'visual_review']
+-- Description: Create a visual review instance, with a suprocess_invocation_id
+
+insert into visual_review_instance(
+  suprocess_invocation_id,
+  visual_review_reason,
+  visual_review_scheduler,
+  visual_review_num_series,
+  when_visual_review_scheduled
+) values (
+  ?, ?, ?, ?, now()
+)
