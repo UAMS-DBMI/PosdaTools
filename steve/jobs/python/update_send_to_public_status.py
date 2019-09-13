5ac49eea5953cba102f9102e6af9f49c7a145108
Sonya Utecht <sutecht@uams.edu>
2019-09-13 10:38:48 -0500

    steve fix

--- steve/jobs/python/update_send_to_public_status.py
+++ steve/jobs/python/update_send_to_public_status.py
@@ -3,13 +3,13 @@ import requests
 
 my_subprocesses = []
 
-r = requests.get('http://web/papi/v1/vrstatus/find_vr_ready_to_begin_status_updates')
+r = requests.get('http://web/papi/v1/send_to_public_status/find_send_ready_to_begin_status_updates')
 resp = r.json()
 
 
 for record in resp:
-    #get review summary for vr
-    r = requests.get("http://web/papi/v1/vrstatus/get_success_percentage_for_send/{}".format(record['subprocess_invocation_id']))
+    #get review summary for record
+    r = requests.get("http://web/papi/v1/send_to_public_status/get_success_percentage_for_send/{}".format(record['subprocess_invocation_id']))
     resp2 = r.json()
 
     my_status = " "
@@ -19,8 +19,8 @@ for record in resp:
     if (my_status == " false 0% true 100% " or " true 100% false 0% "):
         my_status = "Complete"
 
-    #update activity_status for vr
-    r = requests.get("http://web/papi/v1/vrstatus/update_activity_status/{}/{}".format(vr['subprocess_invocation_id'],my_status))
+    #update activity_status for record
+    r = requests.get("http://web/papi/v1/send_to_public_status/update_activity_status/{}/{}".format(record['subprocess_invocation_id'],my_status))
     if (my_status == "Complete"):
-        requests.get("http://web/papi/v1/vrstatus/finish_activity_status/{}".format(vr['subprocess_invocation_id']))
+        requests.get("http://web/papi/v1/send_to_public_status/finish_activity_status/{}".format(record['subprocess_invocation_id']))
     resp4 = r.json()
