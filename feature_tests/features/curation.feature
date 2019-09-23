b19568b29a8282166126be0e3cb251d2c315bd03
Sonya Utecht <sutecht@uams.edu>
2019-09-23 15:11:59 -0500

    steps working towards creating an activity teimpoint

--- feature_tests/features/curation.feature
+++ feature_tests/features/curation.feature
@@ -15,14 +15,26 @@ Feature: Curation workflow test using RSNA
     Then one new activity should exist in dropdown
 
   Scenario: create activity timepoint
-    Given RSNA data was imported under the name "RSNA"
+    Given Test data was imported
       And an activity exists
       And Your browser is allowing popups
     When we open dbif
       And we click activity
       And we count the existing activities
       And we select an activity
-      And we select ActivityOperations
-      And we click Create Activity Timepoint from Import Name
-      And we input the required parameters, Expand, and Start Subprocess
-    Then we recieve an inbox notice of success
+      And we select Queries
+      And we click Search radiobutton
+      And We add parameter SeriesByMatchingImportEventsWithEventInfo
+      And we click Search button
+      And we click foreground
+      And we add SeriesByMatchingImportEventsWithEventInfo parameters
+      And we click Query
+      And we Click CreateActivityTimepointFromSeriesList
+      And we input the parameters including the ID of your Activity
+      And we Click Expand
+      And we Click Start Subprocess
+      And we close popup and switch windows
+      And we file the inbox message
+      And we click activity
+      And we select Timeline
+    Then we see a timeline message with correct file count
--- feature_tests/features/steps/curation.py
+++ feature_tests/features/steps/curation.py
@@ -1,7 +1,7 @@
 import time
 from selenium.webdriver.support.ui import Select
 
-@given(u'RSNA data was imported under the name "RSNA"')
+@given(u'Test data was imported')
 def step_impl(context):
     #do later
     pass
@@ -49,7 +49,7 @@ def step_impl(context):
     activity = context.browser.find_element_by_link_text("Activity")
     activity.click()
     time.sleep(2)
-    assert "Activities" in context.browser.find_element_by_xpath("//h2").text
+
 
 @when(u'we count the existing activities')
 def step_impl(context):
@@ -59,7 +59,7 @@ def step_impl(context):
 @when(u'we create an activity')
 def step_impl(context):
     desc_entry = context.browser.find_element_by_id("newActivity")
-    desc_entry.send_keys("RSNA Activity")
+    desc_entry.send_keys("Test Curation")
     savebutton = context.browser.find_element_by_xpath("//input[@value='Save']")
     savebutton.click()
 
@@ -94,7 +94,7 @@ def step_impl(context):
 @when(u'we input the required parameters, Expand, and Start Subprocess')
 def step_impl(context):
     import_name_input = context.browser.find_element_by_id("import_nameEntryBox")
-    import_name_input.send_keys("RSNA")
+    import_name_input.send_keys("Test Curation Import")
     comment_input = context.browser.find_element_by_id("commentEntryBox")
     comment_input.send_keys("Functional Testing")
     expandbutton = context.browser.find_element_by_xpath("//input[@value='Expand']")
@@ -114,3 +114,100 @@ def step_impl(context):
     inbox = context.browser.find_element_by_link_text("Inbox")
     inbox.click()
     time.sleep(2)
+
+@when(u'we select Queries')
+def step_impl(context):
+    selector = Select(context.browser.find_element_by_id("SetActivityMode"))
+    selector.select_by_value("3")
+    time.sleep(2)
+
+@when(u'we click Search radiobutton')
+def step_impl(context):
+    searchradio = context.browser.find_element_by_xpath("//input[@type='radio'][@value='search']")
+    searchradio.click()
+
+@when(u'We add parameter SeriesByMatchingImportEventsWithEventInfo')
+def step_impl(context):
+    nameparam = context.browser.find_element_by_xpath("//input[@name='NewNameMatchList']")
+    nameparam.send_keys("SeriesByMatchingImportEventsWithEventInfo")
+
+@when(u'we click Search button')
+def step_impl(context):
+    searchbutton = context.browser.find_element_by_xpath("//input[@type='button'][@value='search']")
+    searchbutton.click()
+
+@when(u'we click foreground')
+def step_impl(context):
+    button = context.browser.find_element_by_xpath("//input[@type='button'][@value='foreground']")
+    button.click()
+
+@when(u'we add SeriesByMatchingImportEventsWithEventInfo parameters')
+def step_impl(context):
+    param1 = context.browser.find_element_by_xpath("//th[text()='import_comment_like']/../td/input")
+    param1.send_keys("%") #FIX
+    param2 = context.browser.find_element_by_xpath("//th[text()='import_type_like']/../td/input")
+    param2.send_keys("%") #FIX
+
+@when(u'we click Query')
+def step_impl(context):
+    button = context.browser.find_element_by_xpath("//input[@type='button'][@value='query']")
+    button.click()
+
+@when(u'we Click CreateActivityTimepointFromSeriesList')
+def step_impl(context):
+    button = context.browser.find_element_by_xpath("//input[@type='button'][@value='CreateActivityTimepointFromSeriesList']")
+    button.click()
+    time.sleep(3)
+    context.browser.switch_to.window(context.browser.window_handles[2])
+
+@when(u'we input the parameters including the ID of your Activity')
+def step_impl(context):
+    param1 = context.browser.find_element_by_id("activity_idEntryBox")
+    param1.send_keys("4") #FIX
+    param2 = context.browser.find_element_by_id("commentEntryBox")
+    param2.send_keys("feature testing")
+
+
+@when(u'we Click Expand')
+def step_impl(context):
+    button = context.browser.find_element_by_xpath("//input[@type='button'][@value='Expand']")
+    button.click()
+    time.sleep(2)
+
+@when(u'we Click Start Subprocess')
+def step_impl(context):
+    button = context.browser.find_element_by_xpath("//input[@type='button'][@value='Start Subprocess']")
+    button.click()
+    time.sleep(2)
+
+@when(u'we close popup and switch windows')
+def step_impl(context):
+    button = context.browser.find_element_by_xpath("//button[@type='button']")
+    button.click()
+    context.browser.switch_to.window(context.browser.window_handles[1])
+    time.sleep(5)
+
+@when(u'we file the inbox message')
+def step_impl(context):
+    inbox = context.browser.find_element_by_partial_link_text("Inbox")
+    inbox.click()
+    time.sleep(2)
+    button = context.browser.find_element_by_xpath("//input[@type='button']") # top button
+    button.click()
+    fbutton = context.browser.find_element_by_xpath("//button[normalize-space(.)='File this message']")
+    fbutton.click()
+    ybutton =  context.browser.find_element_by_xpath("//button[normalize-space(.)='Yes']")
+    ybutton.click()
+    time.sleep(2)
+
+
+@when(u'we select Timeline')
+def step_impl(context):
+    selector = Select(context.browser.find_element_by_id("SetActivityMode"))
+    selector.select_by_value("3")
+    time.sleep(2)
+
+
+@then(u'we see a timeline message with correct file count')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then we see a timeline message with correct file count')
