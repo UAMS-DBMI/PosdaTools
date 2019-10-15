a0826035d73241f17b818e52c4c3f41b35332c4a
Joseph Utecht <joseph.utecht@gmail.com>
2019-10-15 13:15:01 -0500

    api calls working

--- web/showmecancer/src/app/app.component.ts
+++ web/showmecancer/src/app/app.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+  constructor(private router:Router){ }
+
+  public home(){
+    this.router.navigate(['/']);
+  }
+}
