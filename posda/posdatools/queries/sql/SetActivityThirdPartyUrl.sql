2bdff2454e0200216668155290851e6f859a2c25
Bill Bennett <wcbennett@uams.edu>
2019-10-25 10:53:07 -0500

    Added Third Party URL interface to ActivityBasedCuration
    This resolves PT-791

---
+++ posda/posdatools/queries/sql/SetActivityThirdPartyUrl.sql
@@ -0,0 +1,12 @@
+-- Name: SetActivityThirdPartyUrl
+-- Schema: posda_queries
+-- Columns: []
+-- Args: ['third_party_analysis_url', 'activity_id']
+-- Tags: ['activity_timepoint_support', 'activity_support']
+-- Description: Set the third_party_analysis_url in activity_table
+--
+
+update activity set
+  third_party_analysis_url = ?
+where
+  activity_id = ?
