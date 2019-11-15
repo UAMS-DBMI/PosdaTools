31d0a2c0f12b806c8827cf98d31d2624f6394ed5
Quasar Jarosz <jjarosz@uams.edu>
2019-11-15 08:41:34 -0600

    Make dump_dicom.sh real

--- posda/fastapi/app/dump_dicom.sh
+++ posda/fastapi/app/dump_dicom.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo "some tests"
-echo "data and stuff here"
+echoerr() { echo "$@" 1>&2; }
+
+POSDA_ROOT=/home/posda/posdatools
+
+#PERL5LIB=$POSDA_ROOT/Posda/include $POSDA_ROOT/Posda/bin/DumpDicom.pl $1
+PATH=$PATH:$POSDA_ROOT/Posda/bin
+
+
+echoerr "Dumping file $1"
+PERL5LIB=$POSDA_ROOT/Posda/include $POSDA_ROOT/Posda/bin/contrib/IheDumpFile.pl $1
