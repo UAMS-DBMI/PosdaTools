c142dfd611900d65f4a510a39f7d2f1c4e973bb3
Bill Bennett <wcbennett@uams.edu>
2020-03-27 07:34:42 -0500

    Fixed a bug in RetryFailedProjections.pl and added a button to invoke it to VisualReviewStatusDetails

--- posda/posdatools/ActivityBasedCuration/include/ActivityBasedCuration/ButtonDefinition.pm
+++ posda/posdatools/ActivityBasedCuration/include/ActivityBasedCuration/ButtonDefinition.pm
@@ -426,6 +426,15 @@ EOF
       VisualReviewStatusDetails => 1,
     },
   },
+  qbp_RetryFailedProjections => {
+    caption => "Retry Failed Projections",
+    spreadsheet_operation => "RetryFailedProjections",
+    operation => "OpenNewTableLevelPopup",
+    obj_class => "Posda::NewerProcessPopup",
+    queries => {
+      VisualReviewStatusDetails => 1,
+    },
+  },
 );
 # BackgroundDoseLinkageCheck            │ GetDoses                                  │ Posda::NewerProcessPopup
 # BackgroundPhiScan                     │ DistinctSeriesByCollection                │ Posda::NewerProcessPopup
