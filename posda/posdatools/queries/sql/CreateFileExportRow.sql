4b61591d03f6924ed1b29cd002ee2e8ccae4f2c9
Bill Bennett <wcbennett@uams.edu>
2020-03-27 10:59:27 -0500

    Added database migrations and some other miscellaneous stuff

--- posda/posdatools/queries/sql/CreateFileExportRow.sql
+++ posda/posdatools/queries/sql/CreateFileExportRow.sql
@@ -8,10 +8,14 @@
 
 insert into file_export (
 <<<<<<< HEAD
+<<<<<<< HEAD
  export_event_id, file_id, when_queued
 =======
  export_even_id, file_id, when_qiueued
 >>>>>>> Initial working copy of ExportTimepoint operation
+=======
+ export_event_id, file_id, when_queued
+>>>>>>> Added database migrations and some other miscellaneous stuff
 ) values (
   ?,  ?, now()
 )
--- posda/posdatools/queries/sql/ExportDaemonRequest.sql
+++ posda/posdatools/queries/sql/ExportDaemonRequest.sql
@@ -1,10 +1,14 @@
 -- Name: ExportDaemonRequest
 -- Schema: posda_files
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> Added database migrations and some other miscellaneous stuff
 -- Columns: []
 -- Args: ['request_status', 'export_event_id']
 -- Tags: ['export_event']
 -- Description: submit a request to Export Daemon
+<<<<<<< HEAD
 =======
 -- Columns: ['file_id']
 -- Args: []
@@ -15,13 +19,19 @@
 -- It is intended for short date ranges (i.e. "What came in last night?" or "What came in last month?")
 -- (Ignore this line, it is a test!)
 >>>>>>> Initial working copy of ExportTimepoint operation
+=======
+>>>>>>> Added database migrations and some other miscellaneous stuff
 --
 
 update export_event set
   request_status = ?,
   request_pending = true
 <<<<<<< HEAD
+<<<<<<< HEAD
 where export_event_id = ?
 =======
 where export_event_id
 >>>>>>> Initial working copy of ExportTimepoint operation
+=======
+where export_event_id = ?
+>>>>>>> Added database migrations and some other miscellaneous stuff
