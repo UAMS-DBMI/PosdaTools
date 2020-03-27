137b75ab469c208ae6c3ac004ebae95ca2c9d2eb
Bill Bennett <wcbennett@uams.edu>
2020-03-27 10:59:27 -0500

    Added database migrations and some other miscellaneous stuff

--- posda/posdatools/queries/sql/ExportDaemonRequest.sql
+++ posda/posdatools/queries/sql/ExportDaemonRequest.sql
@@ -1,16 +1,12 @@
 -- Name: ExportDaemonRequest
 -- Schema: posda_files
--- Columns: ['file_id']
--- Args: []
--- Tags: ['AllCollections', 'DateRange', 'Kirk', 'Totals', 'count_queries', 'end_of_month']
--- Description: Get posda totals by date range
--- 
--- **WARNING:**  This query can run for a **LONG** time if you give it a large date range.
--- It is intended for short date ranges (i.e. "What came in last night?" or "What came in last month?")
--- (Ignore this line, it is a test!)
+-- Columns: []
+-- Args: ['request_status', 'export_event_id']
+-- Tags: ['export_event']
+-- Description: submit a request to Export Daemon
 --
 
 update export_event set
   request_status = ?,
   request_pending = true
-where export_event_id
+where export_event_id = ?
