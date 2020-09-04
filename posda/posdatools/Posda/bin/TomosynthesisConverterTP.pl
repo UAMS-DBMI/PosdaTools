312cefefa1e1642bc92643bc9aa934ed2b0d0cac
Sonya Utecht <sutecht@uams.edu>
2020-09-04 13:59:28 -0500

    SRT changes to SCT

--- posda/posdatools/Posda/bin/TomosynthesisConverterTP.pl
+++ posda/posdatools/Posda/bin/TomosynthesisConverterTP.pl
@@ -131,7 +131,7 @@ for my $file (keys %Files){
 
   $ds->Insert("(0054,0220)[0](0054,0222)",$empty);                                                       #ViewCodeSequence - View Modifier Code Sequence
   $ds->Insert("(0054,0220)[0](0008,0100)","R-10226");                                                    #ViewCodeSequence - Code Value
-  $ds->Insert("(0054,0220)[0](0008,0102)","SRT");                                                        #ViewCodeSequence - Coding Scheme Designator
+  $ds->Insert("(0054,0220)[0](0008,0102)","SCT");                                                        #ViewCodeSequence - Coding Scheme Designator
   $ds->Insert("(0054,0220)[0](0008,0104)","medio-lateral oblique");                                      #ViewCodeSequence - Code Meaning
 
 
