4316382705597c1ece524d9670fa553336cc8f94
Bill Bennett <wcbennett@uams.edu>
2020-12-30 13:33:14 -0600

    This commit adds the Background Process Monitor Window

---
+++ posda/posdatools/Posda/include/Posda/BackgroundList.pm
@@ -0,0 +1,212 @@
+package Posda::BackgroundList;
+# 
+#
+use strict;
+
+use Posda::Config ('Config','Database');
+use Posda::DB 'Query';
+use parent 'Posda::PopupWindow';
+
+
+sub SpecificInitialize {
+  my ($self, $params) = @_;
+  $self->{title} = 'Background Process Lister';
+  $self->{params} = $params;
+  $self->{ListMode} = "All";
+  $self->{checkboxes}->{InLast} = 0;
+  Dispatch::Select::Background->new(sub {
+    my($disp) = @_;
+    if($self->{checkboxes}->{Refresh}){
+       $self->AutoRefreshDiv('WorklistContentResponse',
+         'WorklistContentResponse');
+    }
+    $disp->timer(10);
+  })->queue();
+}
+
+sub HeaderResponse{
+  my($this, $http, $dyn) = @_;
+  return $this->RefreshEngine($http, $dyn,'<center><h1><?dyn="title"?></h1></center>');
+
+}
+
+sub ContentResponse {
+  my ($self, $http, $dyn) = @_;
+  $http->queue("<div id=\"WorklistContentResponse\">");
+  $self->WorklistContentResponse($http, $dyn);
+  $http->queue("</div>");
+}
+sub WorklistContentResponse {
+  my ($self, $http, $dyn) = @_;
+  my $q_name = "GetBackgroundJobsAll";
+  my @args;
+  if($self->{ListMode} eq "running"){
+    $q_name = "GetBackgroundJobsRunning";
+  } elsif($self->{ListMode} eq "errored"){
+    $q_name = "GetBackgroundJobsErrored";
+  } elsif($self->{ListMode} eq "finished"){
+    $q_name = "GetBackgroundJobsFinished";
+  } else {
+    $self->{ListMode} = "all";
+  }
+  if($self->{checkboxes}->{InLast}){
+    $q_name = $q_name . "InLast";
+    push(@args, $self->{InLastInterval});
+  }
+  $self->{List} = [];
+  Query($q_name)->RunQuery(sub{
+    push(@{$self->{List}}, $_[0]);
+  }, sub {}, @args);
+  $http->queue("<table class=\"table table-striped\">");
+  $http->queue("<tr><th>sid</th><th>wid</th>" .
+    "<th>bid</th><th>aid</th><th>status</th><th>input</th>" .
+    "<th>stdout</th><th>stderr</th>" .
+    "<th>invoker</th><th>since</th><th>command_line</th>" .
+    "<th>invoked</th><th>notify</th></tr>");
+  for my $i (@{$self->{List}}){
+    $http->queue("<tr>");
+    for my $j (0 .. 4){
+      unless(defined($i->[$j])) {$i->[$j] = "" }
+      $http->queue("<td>$i->[$j]</td>");
+    }
+    $http->queue("<td>");
+    if(defined $i->[5]){
+      $http->queue("<a class=\"btn btn-primary\" " .
+        "href=\"DownloadTextAttachment?obj_path=$self->{path}&" .
+        "file_id=$i->[5]&type=Input\">$i->[5]</a>");
+    }
+    $http->queue("</td>");
+    $http->queue("<td>");
+    if(defined $i->[6]){
+      $http->queue("<a class=\"btn btn-primary\" " .
+        "href=\"DownloadTextAttachment?obj_path=$self->{path}&" .
+        "file_id=$i->[6]&type=Stdout\">$i->[6]</a>");
+    }
+    $http->queue("</td>");
+    $http->queue("<td>");
+    if(defined $i->[7]){
+      $http->queue("<a class=\"btn btn-primary\" " .
+        "href=\"DownloadTextAttachment?obj_path=$self->{path}&" .
+        "file_id=$i->[7]&type=Stderr\">$i->[7]</a>");
+    }
+    for my $j (8 .. $#{$i}){
+      unless(defined($i->[$j])) {$i->[$j] = "" }
+      $http->queue("<td>$i->[$j]</td>");
+    }
+    $http->queue("</tr>");
+  }
+  $http->queue("</table>");
+}
+sub DownloadTextAttachment{
+  my ($self, $http, $dyn) = @_;
+  my $type = $dyn->{type};
+  my $file_id = $dyn->{file_id};
+  my $f_name = "$type" . "_$file_id.txt";
+  my $rows;
+  $http->DownloadHeader("text/csv", "$f_name");
+  my $path;
+  Query('GetFilePath')->RunQuery(sub{
+    $path = $_[0]->[0];
+  }, sub{}, $file_id);
+  open FILE, "<$path";
+  while(my $line = <FILE>){
+    $http->queue($line);
+  }
+  close FILE;
+}
+sub SubprocessInvocationContentResponse {
+  my ($self, $http, $dyn) = @_;
+  $http->queue("Hello World (SubprocessInvocationContentResponse)");
+}
+sub BackgroundSubprocessContentResponse {
+  my ($self, $http, $dyn) = @_;
+  $http->queue("Hello World (BackgroundSubprocessContentResponse)");
+}
+sub ListAll{
+  my ($self, $http, $dyn) = @_;
+  $self->{ListMode} = "all";
+}
+sub ListRunning{
+  my ($self, $http, $dyn) = @_;
+  $self->{ListMode} = "running";
+}
+sub ListErrored{
+  my ($self, $http, $dyn) = @_;
+  $self->{ListMode} = "errored";
+}
+sub ListFinished{
+  my ($self, $http, $dyn) = @_;
+  $self->{ListMode} = "finished";
+}
+sub MenuResponse {
+  my ($self, $http, $dyn) = @_;
+  $http->queue($self->CheckBoxDelegate("checkboxes", "Refresh",
+    $self->{checkboxes}->{Refresh} ? 1: 0,
+    { op => "SetCheckbox",
+      sync => "Update();" }));
+  $http->queue(" Refresh<hr>");
+  $self->NotSoSimpleButton($http, {
+     op => "ListAll",
+     caption => "All",
+     sync => "Update();"
+  });
+  $http->queue("<br>");
+  $self->NotSoSimpleButton($http, {
+     op => "ListRunning",
+     caption => "Running",
+     sync => "Update();"
+  });
+  $http->queue("<br>");
+  $self->NotSoSimpleButton($http, {
+     op => "ListErrored",
+     caption => "Errored",
+     sync => "Update();"
+  });
+  $http->queue("<br>");
+  $self->NotSoSimpleButton($http, {
+     op => "ListFinished",
+     caption => "Finished",
+     sync => "Update();"
+  });
+  $http->queue("<br>");
+  $http->queue("<hr>");
+  $http->queue($self->CheckBoxDelegate("checkboxes", "InLast",
+    $self->{checkboxes}->{InLast} ? 1: 0,
+    { op => "SetCheckbox",
+      sync => "Update();" }));
+  $http->queue("In last:<br>");
+  if($self->{checkboxes}->{InLast}){
+    $self->BlurEntryBox($http, {
+      name => "InLastInterval",
+      op => "SetInLastInterval",
+      id => "InLastInterval",
+      value => "$self->{InLastInterval}"
+    }, "Update();");
+  }
+}
+sub SetInLastInterval{
+  my ($self, $http, $dyn) = @_;
+  $self->{InLastInterval} = $dyn->{value};
+}
+sub SetCheckbox{
+  my ($self, $http, $dyn) = @_;
+  print STDERR "##################\n";
+  for my $i (keys %{$dyn}){
+    print STDERR "dyn{$i} =  $dyn->{$i}\n";
+  }
+  print STDERR "##################\n";
+  my $checked = 0;
+  if($dyn->{checked} eq "true"){
+    $checked = 1;
+  }
+  $self->{$dyn->{group}}->{$dyn->{value}} = $checked;
+}
+sub ScriptButton {
+  my ($self, $http, $dyn) = @_;
+  my $parent = $self->parent;
+  if($parent->can("ScriptButton")){
+    $parent->ScriptButton($http, $dyn);
+  }
+}
+
+1;
---
+++ posda/posdatools/queries/sql/GetBackgroundJobsErrored.sql
@@ -0,0 +1,21 @@
+-- Name: GetBackgroundJobsErrored
+-- Schema: posda_files
+-- Columns: ['subprocess_invocation_id', 'work_id', 'background_subprocess_id', 'activity_id', 'status', 'input_file_id', 'stdout_file_id', 'stderr_file_id', 'invoking_user', 'since_invocation', 'command_line', 'when_invoked', 'user_to_notify']
+-- Args: []
+-- Tags: ['worker_nodes', 'background_jobs']
+-- Description: Work table and related info
+-- 
+
+select
+  subprocess_invocation_id, work_id, background_subprocess_id, activity_id, 
+  status, input_file_id, stdout_file_id, stderr_file_id, invoking_user, 
+  now()-when_invoked as since_invocation, command_line,
+  when_invoked, user_to_notify
+from
+  subprocess_invocation left join work using(subprocess_invocation_id)
+  left join background_subprocess using(subprocess_invocation_id)
+  left join activity_task_status using(subprocess_invocation_id)
+where
+  status = 'errored'
+order by subprocess_invocation_id desc
+   
