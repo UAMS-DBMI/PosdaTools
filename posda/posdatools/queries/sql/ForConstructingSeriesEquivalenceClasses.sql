fbffc6371ab5cbfa1188ce591bf3d93de5ad59d5
Sonya Utecht <sutecht@uams.edu>
2021-01-06 12:39:09 -0600

    implementing same changes as non-worker node version

--- posda/posdatools/queries/sql/ForConstructingSeriesEquivalenceClasses.sql
+++ posda/posdatools/queries/sql/ForConstructingSeriesEquivalenceClasses.sql
@@ -1,11 +1,11 @@
 -- Name: ForConstructingSeriesEquivalenceClasses
 -- Schema: posda_files
 -- Columns: ['series_instance_uid', 'modality', 'series_number', 'laterality', 'series_date', 'dicom_file_type', 'performing_phys', 'protocol_name', 'series_description', 'operators_name', 'body_part_examined', 'patient_position', 'smallest_pixel_value', 'largest_pixel_value', 'performed_procedure_step_id', 'performed_procedure_step_start_date', 'performed_procedure_step_desc', 'performed_procedure_step_comments', 'image_type', 'iop', 'pixel_rows', 'pixel_columns', 'file_id', 'ipp']
--- Args: ['series_instance_uid']
+-- Args: ['series_instance_uid', activity_timepoint_id]
 -- Tags: ['consistency', 'find_series', 'equivalence_classes']
 -- Description: For building series equivalence classes
 
-select distinct 
+select distinct
  series_instance_uid, modality, series_number, laterality, series_date, dicom_file_type,
   performing_phys, protocol_name, series_description,
   operators_name, body_part_examined, patient_position,
@@ -13,14 +13,12 @@ select distinct
   performed_procedure_step_start_date,
   performed_procedure_step_desc, performed_procedure_step_comments, image_type,
   iop, pixel_rows, pixel_columns,
-  file_id,ipp
+  file_id,ipp, activity_timepoint_id
 from
   file_series natural join ctp_file natural join dicom_file
   left join file_image using(file_id)
   left join image using (image_id)
   left join file_image_geometry using (file_id)
   left join image_geometry using (image_geometry_id)
-where series_instance_uid = ? and visibility is null
- 
-
-
+  left join activity_timepoint_file using (file_id)
+  where series_instance_uid = ? and visibility is null and activity_timepoint_id = ?
