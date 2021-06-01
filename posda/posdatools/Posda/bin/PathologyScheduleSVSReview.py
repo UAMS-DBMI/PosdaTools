e008eafacd9dcf6d16ef0db370ea63b3da6120d3
Sonya Utecht <sutecht@uams.edu>
2021-06-01 10:50:03 -0500

    the main code change is commented out. It 'works' but is too resource intensive to be used.

--- posda/posdatools/Posda/bin/PathologyScheduleSVSReview.py
+++ posda/posdatools/Posda/bin/PathologyScheduleSVSReview.py
@@ -6,12 +6,12 @@ import sys
 import os
 
 from tifffile import TiffFile
-from PIL import Image
+from PIL import Image, ImageFile
 from posda.database import Database
 from posda.queries import Query
-#from posda.background.process import BackgroundProcess
+from posda.background.process import BackgroundProcess
 from posda.main.file import insert_file
-
+Image.MAX_IMAGE_PIXELS = None
 
 def process(filepath, original_file,vr_id):
 
@@ -26,11 +26,12 @@ def process(filepath, original_file,vr_id):
                 preview_file_id = file_id)
 
 def main(args):
-    #background = BackgroundProcess(args.background_id, args.notify, args.activity_id)
+    background = BackgroundProcess(args.background_id, args.notify, args.activity_id)
+    background.daemonize()
 
     vr_id = Query("CreatePathologyVisualReviewInstance").get_single_value(
             activity_creation_id = args.activity_id,
-            scheduler = args.notify);
+            scheduler = args.notify)
 
 
     results = []
@@ -38,23 +39,46 @@ def main(args):
             activity_id=args.activity_id):
         results.append((row.file_id, os.path.join(row.root_path, row.rel_path)))
     for (file_id, svsfilepath) in results:
-        mytif = TiffFile(svsfilepath)
-        for i, page in enumerate(mytif.pages):
-            if (i == 1 or page.tags['NewSubfileType'] != 0 ) and (page.size < 5000000):
-                data = page.asarray()
-                str = "/home/posda/cache/created/{}_page{}.jpg".format(file_id,i)
-                im = Image.fromarray(data)
-                im.save(str) #create thumbnail
-                #print("Images Saved! Import Processing!")
-                process(str, file_id,vr_id) #import thumbnail
+        myfilename = Query("SimpleFilenameFetch").get_single_value(file_id = file_id)
+        print("reviewing file " + svsfilepath + ", which is really " + myfilename +". A " + myfilename[-3:] + " file")
+        if (myfilename[-3:].lower() == "svs"):
+            mytif = TiffFile(svsfilepath)
+            for i, page in enumerate(mytif.pages):
+                if (i == 1 or page.tags['NewSubfileType'] != 0 ) and (page.size < 5000000):
+                    data = page.asarray()
+                    str = "/home/posda/cache/created/{}_page{}.jpg".format(file_id,i)
+                    im = Image.fromarray(data)
+                    im.save(str) #create thumbnail
+                    print(f"Creating an svs preview")
+                    process(str, file_id,vr_id) #import thumbnail
+        # elif(myfilename[-3:].lower() == "tif" or myfilename[-4:].lower() == "tiff") :
+        #     mytif = TiffFile(svsfilepath)
+        #     for i, page in enumerate(mytif.pages):
+        #         data = page.asarray()
+        #         str = "/home/posda/cache/created/{}_page{}.jpg".format(file_id,i)
+        #         im = Image.fromarray(data)
+        #         im.save(str) #create copy
+        #         mytif2 = Image.open(str)
+        #         size = (700,700)
+        #         mytif2.thumbnail(size) #change copy into a thumbnail
+        #         mytif2.save(str)
+        #         print(f"Creating a tif preview")
+        #         process(str, file_id,vr_id) #import thumbnail
+        # elif(myfilename[-3:].lower() == "jpg" or myfilename[-4:].lower() == "jpeg" or myfilename[-3:].lower() == "bmp"):
+        #         myimg = Image.open(svsfilepath)
+        #         str = "/home/posda/cache/created/{}_thumb.jpg".format(file_id)
+        #         size = (700,700)
+        #         myimg.thumbnail(size) #change copy into a thumbnail
+        #         myimg.save(str) #save thumbnail
+        #         print(f"Creating a jpg/bmp preview")
+        #         process(str, file_id,vr_id) #import thumbnail
 
-
-#    background.finish(f"Thumbnail files created and imported")
+    background.finish(f"Thumbnail files created and imported")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Takes an Activity and creates the thumbnails for review for the SVS files")
-    #parser.add_argument("background_id")
+    parser.add_argument("background_id")
     parser.add_argument("activity_id")
     parser.add_argument("notify")
     args = parser.parse_args()
