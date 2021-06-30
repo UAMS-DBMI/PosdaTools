529831a45a000e3142cb4c07b310e7a82119049b
Sonya Utecht <sutecht@uams.edu>
2021-06-30 15:32:05 -0500

    GB report needs a lot of work to be more useful, but it is minimally working now

--- web/Kohlrabi/src/GBreport.js
+++ web/Kohlrabi/src/GBreport.js
@@ -1,24 +1,44 @@
 import './App.css';
 import { useFetch } from './useFetch';
 
+
 function GBreport(props) {
 
     var myfilesdiv = [];
+    var filestatus = [];
     const myfiles = useFetch(`/papi/v1/pathology/review/${props.VRindex}`);
     if(myfiles){
-      myfilesdiv = myfiles.map((row, i) =>
-        <tr>
-          <td>${row.file_name}</td><td>${row.good}</td>
-        </tr>
-       );
+      let sortedfiles = [...myfiles];
+      sortedfiles.sort((a, b) => {
+        if (a.good < b.good) {
+          return -1;
+        }
+        if (a.good > b.good) {
+          return 1;
+        }
+        return 0;
+      });
+
+        filestatus = sortedfiles.map((row, i) => {
+          var status = "Unreviewed";
+          if (row.good === true){
+            status = "Good";
+          }else if(row.good === false){
+            status = "Bad";
+          }
+          return  <div key={i}> <tr><td> {row.file_name} </td> <td>{status}</td> </tr></div>
+      });
     }
+
+
 
     return (
       <div>
           <div>
             <h1>Files as currently labeled</h1>
             <table>
-              {myfilesdiv}
+            <tr><th>File</th><th>Status</th></tr>
+              {filestatus}
             </table>
           </div>
       </div>
