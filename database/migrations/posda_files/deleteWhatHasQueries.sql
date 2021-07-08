9389c46c2e8ca9f362e18ffdaf1b0b954bde1693
Sonya Utecht <sutecht@uams.edu>
2021-07-08 14:56:31 -0500

    quasar pointed out other sites may have custom queries, so names should be specific

--- database/migrations/posda_files/deleteWhatHasQueries.sql
+++ database/migrations/posda_files/deleteWhatHasQueries.sql
@@ -3,4 +3,9 @@
   Remove obsolete queries that cause confusion for users
 */
 
-delete from queries where name like 'WhatHas%'
+delete from queries where name = 'WhatHasComeInRecently';
+delete from queries where name = 'WhatHasComeInRecentlyByCollectionLike';
+delete from queries where name = 'WhatHasComeInRecentlyByCollectionLikeAndFileInPosdaCount';
+delete from queries where name = 'WhatHasComeInRecentlyWithSubject';
+delete from queries where name = 'WhatHasComeInRecentlyWithSubjectByCollectionLike';
+delete from queries where name = 'WhatHasComeInRecentlyWithSubjectByCollectionLikeAndFileInPosdaCount';
