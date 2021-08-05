18c6ff1906b862a78fad994748451e1f99f83ff1
Quasar Jarosz <jjarosz@uams.edu>
2021-08-05 17:14:18 -0500

    Add missing digest file

---
+++ posda/fastapi/app/papi/util/digest.py
@@ -0,0 +1,12 @@
+import hashlib
+
+def md5sum_file(filename):
+    """Calculate the md5sum of filename, return (size, digest)"""
+    bytes_read = 0
+    with open(filename, "rb") as f:
+        file_hash = hashlib.md5()
+        while chunk := f.read(8192):
+            file_hash.update(chunk)
+            bytes_read = len(chunk)
+
+    return bytes_read, file_hash.hexdigest()
