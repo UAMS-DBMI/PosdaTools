d910a3f28f8d6ed1466c06d80454802bac6d6665
Sonya Utecht <sutecht@uams.edu>
2021-08-08 13:09:47 -0500

    starting to get some values into the db now, but having some errors. new queries locally, but not pushing yet, want to wait til they need fewer modifications before pushing

--- posda/posdatools/Posda/bin/SR_phiscan.pl
+++ posda/posdatools/Posda/bin/SR_phiscan.pl
@@ -29,6 +29,11 @@ my $file_id;
 my $q = Query('SeriesForFile');
 my $q2 = Query('FilesInSeriesWithPath');
 
+
+
+
+
+
 sub GetPaths{
   my($content) = @_;
   for my $i (@{$content}){
@@ -46,6 +51,18 @@ sub GetPaths{
   }
 }
 
+my $create_scan = PosdaDB::Queries->GetQueryInstance("SRCreateScanInstance");
+my $create_path = PosdaDB::Queries->GetQueryInstance("SRCreatePathSeen");
+my $get_value = PosdaDB::Queries->GetQueryInstance("GetSimpleValueSeen");
+my $create_value = PosdaDB::Queries->GetQueryInstance("CreateSimpleValueSeen");
+my $get_path = PosdaDB::Queries->GetQueryInstance("SRGetPathSeen");
+my $get_value_id=
+  PosdaDB::Queries->GetQueryInstance("GetSimpleValueSeenId");
+my $create_occurance =
+  PosdaDB::Queries->GetQueryInstance("SRCreatePathValueOccurance");
+
+
+
 Query('LatestActivityTimepointsForActivity')->RunQuery(sub{
   my($row) = @_;
   my($activity_id, $activity_created,
@@ -58,6 +75,12 @@ Query('FileIdsByActivityTimepointId')->RunQuery(sub {
   my($row) = @_;
   $Files{$row->[0]} = 1;
 }, sub {}, $ActTpId);
+
+my $scan_id;
+$create_scan->RunQuery(sub {
+    my($row) = @_;
+    $scan_id = $row->[0];
+  }, sub{}, $act_id);
 
 
 
@@ -87,13 +110,48 @@ for  $file_id(keys %Files){
 
     my $content = $ParsedSR->{content};
     GetPaths $content;
+    # for my $path(sort keys %Paths){
+    #   for my $v (sort keys %{$Paths{$path}}){
+    #     #print "$path|$v\n";
+    #     #if($path =~ /^(.*) \(.*\)$/){ $path = $1 }
+    #     $path =~ s/\s\([^)]+\)//g;
+    #     $v =~ s/\s\([^)]+\)//g;
+    #     print "$path|$v\n";
+    #   }
+    # }
+
+    #while(my $line = <SUBP>){
     for my $path(sort keys %Paths){
-      for my $v (sort keys %{$Paths{$path}}){
-        #print "$path|$v\n";
-        #if($path =~ /^(.*) \(.*\)$/){ $path = $1 }
-        $path =~ s/\s\([^)]+\)//g;
-        $v =~ s/\s\([^)]+\)//g;
-        print "$path|$v\n";
+      my $tag_id;
+      my $path_id;
+      my $vr;
+
+      $get_path->RunQuery(sub {
+        my($row) = @_;
+        $path_id = $row->[0];
+      }, sub {}, $path, $vr);
+      unless(defined $path_id){
+        $create_path->RunQuery(sub {
+          my($row) = @_;
+          $path_id = $row->[0];
+        }, sub {},
+          $path, $vr);
       }
+      my $value_id;
+      my $v = $Paths{$path};
+      $get_value->RunQuery(sub {
+        my($row) = @_;
+        $value_id = $row->[0];
+      }, sub {}, $v);
+      unless(defined $value_id){
+        $create_value->RunQuery(sub {}, sub {},
+          $v);
+        $get_value_id->RunQuery(sub {
+          my($row) = @_;
+          $value_id = $row->[0];
+        }, sub {} );
+      }
+      $create_occurance->RunQuery(sub {}, sub {},
+        $path_id, $value_id, $seriesId, $scan_id)
     }
   };
