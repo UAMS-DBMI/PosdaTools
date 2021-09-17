43e35ad5d686ab7e6f2e8490706fed2f5dd29a34
Quasar Jarosz <jjarosz@uams.edu>
2021-09-16 22:34:51 -0500

    add activity_id/activity_timepoint_id to series api endpoint
    This commit adds the query parameters activity_id and
    activity_timepoint_id to the /v1/series/{series_instance_uid}/files
    endpoint. Either can be set, with activity_timepoint_id taking
    precedence.
    
    Additionally, the series_instance_uid can include the
    activity_timepoint_id after a colon, for backwards compatibility with
    some other tools.

--- posda/fastapi/app/papi/routes/series.py
+++ posda/fastapi/app/papi/routes/series.py
@@ -40,22 +40,99 @@ async def get_single_series(series_instance_uid: str, db: Database = Depends()):
 
 
 @router.get("/{series_instance_uid}/files")
-async def get_all_files(series_instance_uid: str, db: Database = Depends()):
-    query = """
-        select file_id
-        from
-            file_series
-            natural join file_sop_common
-            natural join ctp_file
-        where series_instance_uid = $1
-          and visibility is null
-        order by
-            -- sometimes instance_number is empty string or null
-            case instance_number
-                when '' then '0'
-                when null then '0'
-                else instance_number
-            end::int
+async def get_all_files(series_instance_uid: str, 
+                        activity_id: int = None,
+                        activity_timepoint_id: int = None,
+                        db: Database = Depends()):
+    """Get a list of all files in the series
+
+
+    If activity_id and activity_timepoint_id are unset, returns all files 
+    in the series that are not hidden.
+
+    If activity_id is set, return all files (hidden or not) from the series
+    which are in the latest timepoint in the activity.
+
+    If activity_timepoint_id is set, return all files (hidden or not) from
+    the series which are in the given timepoint.
+
+    If both activity_id and activity_timepoint_id are set, 
+    activity_timepoint_id takes precedence.
+
+    Additionally, an activity_timepoint_id can be given by appending it
+    to the series after a colon, such as:
+
+    "1.2.3.4553452342234:13"
+     
+    This will take precedence over all other timepoint directives.
+
     """
+    if ":" in series_instance_uid:
+        series_instance_uid, activity_timepoint_id = series_instance_uid.split(':')
+
+    if activity_timepoint_id is not None:
+        query = f"""
+            with timepoint_files as (
+                select file_id
+                from activity_timepoint_file
+                where activity_timepoint_id = {activity_timepoint_id}
+            )
+            select file_id
+            from
+                timepoint_files
+                natural join file_series
+                natural join file_sop_common
+            where series_instance_uid = $1
+            order by
+                -- sometimes instance_number is empty string or null
+                case instance_number
+                    when '' then '0'
+                    when null then '0'
+                    else instance_number
+                end::int
+        """
+    elif activity_id is not None:
+        query = f"""
+            with timepoint_files as (
+                select file_id
+                from activity_timepoint_file
+                where
+                    activity_timepoint_id = (
+                        select max(activity_timepoint_id)
+                        from activity_timepoint
+                        where activity_id = {activity_id}
+                    )
+            )
+            select file_id
+            from
+                timepoint_files
+                natural join file_series
+                natural join file_sop_common
+            where series_instance_uid = $1
+            order by
+                -- sometimes instance_number is empty string or null
+                case instance_number
+                    when '' then '0'
+                    when null then '0'
+                    else instance_number
+                end::int
+        """
+    else:
+        query = """
+            select file_id
+            from
+                file_series
+                natural join file_sop_common
+                natural join ctp_file
+            where series_instance_uid = $1
+              and visibility is null
+            order by
+                -- sometimes instance_number is empty string or null
+                case instance_number
+                    when '' then '0'
+                    when null then '0'
+                    else instance_number
+                end::int
+        """
 
     return {"file_ids": [x[0] for x in await db.fetch(query, [series_instance_uid])]}
