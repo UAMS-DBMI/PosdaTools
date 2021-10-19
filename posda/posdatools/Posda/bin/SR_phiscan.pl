8c37c47c4ce3ab5ca993ecb85ef629707a3aa4d0
sutecht <sutecht@uams.edu>
2021-10-19 14:08:27 -0500

    copyright changed

--- posda/posdatools/Posda/bin/SR_phiscan.pl
+++ posda/posdatools/Posda/bin/SR_phiscan.pl
@@ -1,6 +1,5 @@
 #!/usr/bin/perl -w
 #
-#Copyright 2008, Bill Bennett
 # Part of the Posda package
 # Posda may be copied only under the terms of either the Artistic License or the
 # GNU General Public License, which may be found in the Posda Distribution,
