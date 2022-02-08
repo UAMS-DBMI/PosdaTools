b690d55a8d626c9794f3dee81515c8e2642dc8a9
Quasar Jarosz <jjarosz@uams.edu>
2022-02-08 12:12:43 -0600

    Remove report generation to resolve crashing issue

--- posda/posdatools/Posda/bin/CreateActivityTimepointFromImportEventIdAll.pl
+++ posda/posdatools/Posda/bin/CreateActivityTimepointFromImportEventIdAll.pl
@@ -2,7 +2,6 @@
 use strict;
 use Posda::DB 'Query';
 use Posda::BackgroundProcess;
-use Posda::ActivityInfo;
 use Debug;
 my $dbg = sub { print @_ };
 $| = 1; # this should probably be at the top of the script, maybe in the lib?
@@ -71,40 +70,8 @@ for my $file_id (keys %Files){
 my $creation_time = time;
 my $creation = $creation_time - $start_creation;
 $background->WriteToEmail("Created tables in $creation seconds.\n");
-$background->WriteToEmail("Preparing reports.\n");
-##################################
-my $ActInfo = Posda::ActivityInfo->new($act_id);
-my $when = `date`;
-chomp $when;
-my $TpFileInfo = $ActInfo->GetFileInfoForTp($act_time_id);
-my $new_fh = $ActInfo->MakeFileHierarchyFromInfo(
-  $TpFileInfo);
-my $rpt1 = $background->CreateReport("Timepoint Report");
-$rpt1->print("Timepoint Creation Report\r\n");
-$rpt1->print("key,value\r\n");
-$rpt1->print("report,\"Timepoint Report\"\r\n");
-$rpt1->print("script,\"$0\"\r\n");
-$rpt1->print("tp_id,$act_time_id\r\n");
-$rpt1->print("activity_id,$act_id\r\n");
-$rpt1->print("when,$when\r\n");
-$rpt1->print("who,$notify\r\n");
-$rpt1->print("\r\n");
-$ActInfo->PrintHierarchyReport($rpt1, $new_fh);
-
-my $new_cfh = $ActInfo->MakeCondensedHierarchyFromInfo(
-  $TpFileInfo);
-my $rpt4 = $background->CreateReport("Condensed Timepoint Report");
-$rpt4->print("Condensed Timepoint Creation Report");
-$rpt4->print("key,value\r\n");
-$rpt4->print("report,\"Condensed Timepoint Content Report\"\r\n");
-$rpt4->print("script,\"$0\"\r\n");
-$rpt4->print("tp_id,$act_time_id\r\n");
-$rpt4->print("activity_id,$act_id\r\n");
-$rpt4->print("when,$when\r\n");
-$rpt4->print("who,$notify\r\n");
-$rpt4->print("\r\n");
-$ActInfo->PrintCondensedHierarchyReport($rpt4, $new_cfh);
-##################################
+$background->WriteToEmail("NOTE: Reports are no longer included in this email." .
+  "Users should rely on the timepoint report queries instead.\n");
 my $num_files = keys %Files;
 $background->Finish("Created activity_timepoint $act_time_id with" .
   " $num_files files");
