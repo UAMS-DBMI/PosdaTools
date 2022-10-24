db5347c95c833d9a5530a02596ccb6b761967c8c
Quasar Jarosz <jjarosz@uams.edu>
2022-10-24 09:54:26 -0500

    Send file_id as posdaTransferId when submitting files to NBIA

--- ream/ream.py
+++ ream/ream.py
@@ -99,6 +99,7 @@ def _submit_file(f):
                'uri': f.filename,
                'thirdPartyAnalysis': tpa,
                'descriptionURI': tpa_url,
+               'posdaTransferId': file_id,
                }
     headers = {
         "Authorization": "Bearer {}".format(TOKEN),
