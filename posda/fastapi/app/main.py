1a3efbd663ee7416a4fb53a7f8382748abc32f98
Quasar Jarosz <jjarosz@uams.edu>
2022-11-30 13:43:01 -0600

    Add new sysstatus/system endpoint to the API

--- posda/fastapi/app/main.py
+++ posda/fastapi/app/main.py
@@ -23,6 +23,7 @@ from papi.routes import pathology
 from papi.routes import work
 from papi.routes import deface
 from papi.routes import edits
+from papi.routes import sysstatus
 
 # configure importer
 importer.FILE_STORAGE_PATH = os.environ.get(
@@ -70,6 +71,7 @@ router_v1.include_router(pathology.router, prefix="/pathology")
 router_v1.include_router(work.router, prefix="/work")
 router_v1.include_router(deface.router, prefix="/deface")
 router_v1.include_router(edits.router, prefix="/edits")
+router_v1.include_router(sysstatus.router, prefix="/sysstatus")
 
 app.include_router(auth.router)
 app.include_router(router_v1, prefix="/v1")
---
+++ posda/fastapi/app/papi/routes/sysstatus.py
@@ -0,0 +1,30 @@
+from fastapi import Depends, APIRouter, HTTPException
+from pydantic import BaseModel
+from typing import List
+import datetime
+from starlette.responses import Response, FileResponse, HTMLResponse
+from .auth import logged_in_user, User
+from ..util import Database, asynctar
+
+router = APIRouter()
+
+@router.get("/")
+async def test():
+    raise HTTPException(detail="test error, not allowed", status_code=401)
+
+
+@router.get("/system/{system_key}")
+async def get_reviewed_percentage_for_vr(system_key: str, 
+                                         db: Database = Depends()):
+    # query = """\
+    #     select
+    #         ('' || processing_status::text || ' ' || ((count(*)/(select  sum(c) as t from (select count(*) as c from image_equivalence_class where visual_review_instance_id = $1 ) as sum_table)::float) * 100.0)::int || '%')::text as summary
+    #     from
+    #         image_equivalence_class
+    #     where
+    #         visual_review_instance_id = $1
+    #     group by
+    #         processing_status
+    # """
+    # return await db.fetch(query,[visual_review_instance_id])
+    return HTMLResponse(f"<i>Test Message for {system_key}</i>")
