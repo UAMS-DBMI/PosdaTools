172de7c6016535cffc11551366205b77923272d0
Quasar Jarosz <quasar@uams.edu>
2024-04-11 15:29:24 -0500

    Modify WritePart10 to use Explicit even if Implicit is specified when saving
    This commit also adds a new "enum" style mapping of Transfer Syntax UIDs
    to their names, available in Posda::UID.

--- posda/posdatools/Posda/include/Posda/UID.pm
+++ posda/posdatools/Posda/include/Posda/UID.pm
@@ -96,4 +96,33 @@ sub GetUID{
    return GetPosdaRoot($h);
   }
 };
+
+use constant {
+  ImplicitVRLittleEndian => '1.2.840.10008.1.2',
+  ExplicitVRLittleEndian => '1.2.840.10008.1.2.1',
+  DeflatedExplicitVRLittleEndian => '1.2.840.10008.1.2.1.99',
+  ExplicitVRBigEndian => '1.2.840.10008.1.2.2',
+  JPEGBaseline8Bit => '1.2.840.10008.1.2.4.50',
+  JPEGExtended12Bit => '1.2.840.10008.1.2.4.51',
+  JPEGLosslessP14 => '1.2.840.10008.1.2.4.57',
+  JPEGLosslessSV1 => '1.2.840.10008.1.2.4.70',
+  JPEGLSLossless => '1.2.840.10008.1.2.4.80',
+  JPEGLSNearLossless => '1.2.840.10008.1.2.4.81',
+  JPEG2000Lossless => '1.2.840.10008.1.2.4.90',
+  JPEG2000 => '1.2.840.10008.1.2.4.91',
+  JPEG2000MCLossless => '1.2.840.10008.1.2.4.92',
+  JPEG2000MC => '1.2.840.10008.1.2.4.93',
+  MPEG2MPML => '1.2.840.10008.1.2.4.100',
+  MPEG2MPHL => '1.2.840.10008.1.2.4.101',
+  MPEG4HP41 => '1.2.840.10008.1.2.4.102',
+  MPEG4HP41BD => '1.2.840.10008.1.2.4.103',
+  MPEG4HP422D => '1.2.840.10008.1.2.4.104',
+  MPEG4HP423D => '1.2.840.10008.1.2.4.105',
+  MPEG4HP42STEREO => '1.2.840.10008.1.2.4.106',
+  HEVCMP51 => '1.2.840.10008.1.2.4.107',
+  HEVCM10P51 => '1.2.840.10008.1.2.4.108',
+  RLELossless => '1.2.840.10008.1.2.5',
+};
+
+
 1
