fdc15c63e985587a117082fb4be33cba3018ca8c
Michael Rutherford <mwrutherford@uams.edu>
2024-09-04 08:33:21 -0500

    Fixed queries

--- posda/posdatools/queries/sql/NiftiReviewCountByActivity.sql
+++ posda/posdatools/queries/sql/NiftiReviewCountByActivity.sql
@@ -1,28 +1,26 @@
 -- Name: NiftiReviewCountByActivity
 -- Schema: posda_files
--- Columns: ['nifti_file_id', 'num_reviews', 'num_reviewers','num_bad','most_recent_timepoint_containing']
+-- Columns: ['nifti_file_id', 'review_status', 'latest_timepoint','num_reviewers']
 -- Args: ['activity_id']
 -- Tags: ['nifti', 'visual_review']
 -- Description: Get a summmary of Nifti visual review status by activity
 --
 
---
-select
-  nifti_file_id, count(b.good_status) as num_reviews,
-  count(distinct b.reviewing_user) as num_reviewers,  
-  count(b.good_status) filter (where not good_status) as num_bad,  
-  most_recent_timepoint_containing
-from nifti_visual_review_files a  
-natural left join nifti_visual_review_status b
-natural join nifti_visual_review_instance c
-join (select file_id, max(e.activity_timepoint_id) as most_recent_timepoint_containing 
-    from activity d
-    join activity_timepoint e 
-    on d.activity_id = e.activity_id
-    join activity_timepoint_file f 
-    on e.activity_timepoint_id = f.activity_timepoint_id
-group by file_id) as latestAT 
-on a.nifti_file_id = latestAT.file_id
-where activity_id = ?
-group by a.nifti_file_id, most_recent_timepoint_containing
-order by most_recent_timepoint_containing desc
+select nvrf.nifti_file_id,
+       nvrs.review_status,
+       latest_timepoint,
+       count(nvrs.reviewing_user) as num_reviewers
+from nifti_visual_review_files nvrf
+left join nifti_visual_review_status nvrs on nvrs.nifti_file_id = nvrf.nifti_file_id
+join (select a.activity_id,
+             atpf.file_id, 
+             max(atp.activity_timepoint_id) as latest_timepoint 
+	    from activity a
+	    join activity_timepoint atp
+	    on atp.activity_id = a.activity_id
+	    join activity_timepoint_file atpf
+	    on atpf.activity_timepoint_id = atp.activity_timepoint_id
+        group by a.activity_id, atpf.file_id) as latestAT 
+on latestAT.file_id = nvrf.nifti_file_id        
+where latestAT.activity_id = ?
+group by nvrf.nifti_file_id, nvrs.review_status, latest_timepoint
--- posda/posdatools/queries/sql/NiftiVRlogs.sql
+++ posda/posdatools/queries/sql/NiftiVRlogs.sql
@@ -1,13 +1,13 @@
 -- Name: NiftiVRlogs
 -- Schema: posda_files
--- Columns: ['nifti_visual_review_instance_id', 'nifti_file_id', 'good_status', 'reviewing_user', 'review_time']
+-- Columns: ['nifti_visual_review_instance_id', 'nifti_file_id', 'review_status', 'reviewing_user', 'review_time']
 -- Args: ['activity_id']
 -- Tags: ['nifti', 'visual_review']
 -- Description: Get all nifti visual reviews for an activity
 --
 
 select
-  nifti_visual_review_instance_id, nifti_file_id, good_status, reviewing_user, review_time
+  nifti_visual_review_instance_id, nifti_file_id, review_status, reviewing_user, review_time
 from nifti_visual_review_files a  
 natural left join nifti_visual_review_status b
 natural join nifti_visual_review_instance c
