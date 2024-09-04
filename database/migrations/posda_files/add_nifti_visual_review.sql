bc3c5206332e0c863e7ef14cf55c5a9af9b95812
Michael Rutherford <mwrutherford@uams.edu>
2024-09-03 23:29:57 -0500

    Add / update / improve nifti review tables and queries

--- database/migrations/posda_files/add_nifti_visual_review.sql
+++ database/migrations/posda_files/add_nifti_visual_review.sql
@@ -1,4 +1,3 @@
-
 --
 -- Name: add_nifti_visual_review script
 --
@@ -7,19 +6,31 @@
 -- drop table nifti_visual_review_files;
 -- drop table nifti_visual_review_status;
 
+
+alter table activity add primary key (activity_id);
+
+
 create table if not exists nifti_visual_review_instance (
-	nifti_visual_review_instance_id serial not null,
-	activity_id int,
-	scheduler text,
-	scheduled timestamp);
+	nifti_visual_review_instance_id serial not null primary key,
+	activity_id int4 not null,
+	scheduler text not null,
+	scheduled timestamp,
+	foreign key (activity_id) references activity(activity_id)
+);
 
 create table if not exists nifti_visual_review_files (
 	nifti_visual_review_instance_id int not null,
 	nifti_file_id int not null unique,
-	nifti_file_name text);
+	nifti_file_name text,
+    primary key (nifti_visual_review_instance_id, nifti_file_id),
+    foreign key (nifti_visual_review_instance_id) references nifti_visual_review_instance(nifti_visual_review_instance_id),
+	foreign key (nifti_file_id) references file_nifti(file_id)
+);
 
 create table if not exists nifti_visual_review_status (
-	nifti_file_id int4,
-	good_status bool,
+	nifti_file_id int4 not null primary key,
+	review_status text,
 	reviewing_user text,
-	review_time timestamp NULL);
+	review_time timestamp,
+    foreign key (nifti_file_id) references file_nifti(file_id)
+);
