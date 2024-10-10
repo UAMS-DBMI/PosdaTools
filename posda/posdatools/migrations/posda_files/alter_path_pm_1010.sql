0456f5ab8c908741bd0a571efe640d12935148c5
sutecht <sutecht@uams.edu>
2024-10-10 14:48:32 -0700

    migrations

---
+++ posda/posdatools/migrations/posda_files/alter_path_pm_1010.sql
@@ -0,0 +1,3 @@
+ALTER TABLE public.pathology_patient_mapping ADD study_name text NULL;
+ALTER TABLE public.pathology_patient_mapping ADD image_id text NULL;
+ALTER TABLE public.pathology_patient_mapping ADD clinical_trial_subject_id text NULL;
