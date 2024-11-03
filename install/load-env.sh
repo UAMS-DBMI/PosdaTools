bac2f6a0413ca535c5b0f5035d2077dd631e49d3
Quasar Jarosz <quasar@uams.edu>
2024-11-02 22:33:12 -0400

    Rename config dir to not confuse people
    It had temp in the name, but it is not temporary

--- install/load-env.sh
+++ install/load-env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BASE_DIR=/oneposda/install
-CONF_DIR=$BASE_DIR/temp-config
+CONF_DIR=$BASE_DIR/configs
 
 export $(cat $CONF_DIR/*.env | grep -v ^#)
 #source scl_source enable rh-python38
