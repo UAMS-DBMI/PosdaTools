feeaeed67b94f99e480c4b01f16bae4f0f414364
Quasar Jarosz <quasar@uams.edu>
2024-11-25 17:44:27 -0600

    Update IEC frames API with new return schema

--- posda/fastapi/app/papi/routes/iecs.py
+++ posda/fastapi/app/papi/routes/iecs.py
@@ -7,6 +7,10 @@ from starlette.responses import Response, FileResponse
 from .auth import logged_in_user, User
 from ..util import Database
 
+import numpy as np
+from dataclasses import dataclass, asdict
+from collections import defaultdict
+
 router = APIRouter(
     tags=["Image Equivalence Classes (IEC)"],
     dependencies=[logged_in_user]
@@ -50,29 +54,113 @@ async def get_iec_files(iec: int, db: Database = Depends()):
 
 class Frame(BaseModel):
     file_id: int
-    frames: int
+    num_of_frames: int
+
+class FrameResponse(BaseModel):
+    volumetric: bool
+    frames: list[Frame]
+
+
+@dataclass
+class File:
+    file_id: int
+    image_type: list[str]
+    frame_count: int
+    orientation: list
+    position: list
+
+    def from_raw(tup):
+        file_id, image_type, num_frames, iop, ipp = tup
+
+        if num_frames is None:
+            num_frames = 1
+        else:
+            num_frames = int(num_frames)
+
+        image_types = image_type.split('\\')
+
+        # iop and ipp need to be broken into lists
+        iop_parts = iop.split('\\')
+        orientation = [float(i) for i in iop_parts]
+
+        ipp_parts = ipp.split('\\')
+        position = [float(i) for i in ipp_parts]
+
+        return File(file_id, image_types, num_frames, orientation, position)
+
+    def normals(self):
+        return np.cross(self.orientation[:3], self.orientation[3:])
+
+    def projected_position(self):
+        return np.dot(self.position, self.normals())
+
+
+
+def consistent(file_objs) -> tuple[list[File], bool]:
+    """Detect if the list of frames is consistent or not.
+
+    Also returns the list sorted appropriately by projecting 
+    with the iop/ipp
+    """
+
+    # not enough slices to have any idea
+    if len(file_objs) < 2:
+        return False
+
+    sorted_by_proj_position = sorted(file_objs, key=lambda x: x.projected_position())
+
+    all_positions = [i.projected_position() for i in sorted_by_proj_position]
+
+    all_differences = np.diff(all_positions, axis=0)
+
+    consistent_slices = np.allclose(all_differences, all_differences[0], atol=1e-3)
+
+    return (sorted_by_proj_position, consistent_slices)
 
 
 @router.get("/{iec}/frames")
-async def get_iec_frames(iec: int, db: Database = Depends()) -> list[Frame]:
-    query = """
-    select
-        file_id,
-        coalesce(number_of_frames, 1) as frames
-    from
-        image_equivalence_class_input_image
-        natural join file_sop_common
-        natural left join file_image
-        natural left join image
-    where
-        image_equivalence_class_id = $1
-    order by
-        -- sometimes instance_number is empty string or null
-        case instance_number
-            when '' then '0'
-            when null then '0'
-            else instance_number
-        end::int
+async def get_iec_frames(iec: int, db: Database = Depends()) -> FrameResponse:
+    """Get a list of frames (files and frame counts) from this IEC.
+
+    Also returns a guess for if the data is intended to be volumetric.
     """
 
-    return [dict(x) for x in await db.fetch(query, [iec])]
+    query = """
+        select
+            file_id,
+            image_type,
+            coalesce(number_of_frames, 1) as frame_count,
+            iop,
+            ipp
+
+        from
+            image_equivalence_class_input_image
+            natural left join file_image
+            natural left join image
+            natural left join file_image_geometry
+            natural left join image_geometry
+        where
+            image_equivalence_class_id = $1
+    """
+
+    def raw_to_obj(rows):
+        return [File.from_raw(i) for i in rows]
+
+    framelist = raw_to_obj([list(x) for x in await db.fetch(query, [iec])])
+    if len(framelist) < 1:
+        raise HTTPException(detail="no records returned", status_code=404)
+
+    sorted_framelist, consistent_frames = consistent(framelist)
+
+    simplified = [
+        { 
+            "file_id": x.file_id,
+            "num_of_frames": x.frame_count,
+        }
+        for x in sorted_framelist
+    ]
+
+    return {
+        "volumetric": consistent_frames,
+        "frames": simplified,
+    }
