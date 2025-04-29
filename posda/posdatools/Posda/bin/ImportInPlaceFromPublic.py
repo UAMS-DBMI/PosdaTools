b0fcd13fc68a7b2ee6e55bc506c392c8b147b971
Quasar Jarosz <quasar@uams.edu>
2025-04-29 17:29:59 -0500

    Add Site Name parameter to ImportInPlaceFromPublic.py

--- posda/posdatools/Posda/bin/ImportInPlaceFromPublic.py
+++ posda/posdatools/Posda/bin/ImportInPlaceFromPublic.py
@@ -2,11 +2,6 @@
 """
 Import-in-place from Public
 """
-import json
-import os
-import tempfile
-import subprocess
-import hashlib
 import sys
 
 import pydicom
@@ -19,8 +14,8 @@ from posda.background.process import BackgroundProcess
 import argparse
 
 URL = Config.get("internal_api_url") + "/v1/import/"
-OLD_PATH = '/usr/local/apps/ncia/CTP-server/CTP/storage'
-NEW_PATH = '/nas/public/storage'
+OLD_PATH = "/usr/local/apps/ncia/CTP-server/CTP/storage"
+NEW_PATH = "/nas/public/storage"
 
 
 def parse_visibility(s):
@@ -28,7 +23,7 @@ def parse_visibility(s):
         return None
 
     try:
-        parts = s.split(',')
+        parts = s.split(",")
         int_parts = [int(i) for i in parts]
 
         return [str(i) for i in int_parts]
@@ -36,17 +31,18 @@ def parse_visibility(s):
         print("Error converting visibility parameter:", ve)
         return None
 
+
 def printe(*args):
     """Print, but to STDERR"""
     print(*args, file=sys.stderr)
 
+
 def create_import_event(import_comment):
-    r = requests.put(URL + "event", params={
-        'source': import_comment
-    })
+    r = requests.put(URL + "event", params={"source": import_comment})
 
     resp = r.json()
-    return resp['import_event_id']
+    return resp["import_event_id"]
+
 
 def close_import_event(import_event_id):
     r = requests.post(URL + f"event/{import_event_id}/close")
@@ -56,11 +52,14 @@ def close_import_event(import_event_id):
 def add_file(filename, digest, import_event_id):
     """add one file using file_in_place endpoint"""
 
-    r = requests.post(URL + "file_in_place", params={
-        'import_event_id': import_event_id,
-        'localpath': filename,
-        'digest': digest,
-    })
+    r = requests.post(
+        URL + "file_in_place",
+        params={
+            "import_event_id": import_event_id,
+            "localpath": filename,
+            "digest": digest,
+        },
+    )
 
     try:
         resp = r.json()
@@ -75,6 +74,7 @@ def get_xfer_syntax(filename):
         return ds.file_meta.TransferSyntaxUID
     except:
         return None
+
 
 def import_one_file(import_event_id, filename, digest):
     """import one file using the file_in_place endpoint"""
@@ -98,66 +98,98 @@ def import_one_file(import_event_id, filename, digest):
 
     return True
 
-def execute_count_query(cur, collection_name, vis):
+
+def execute_count_query(cur, collection_name, vis, site_name):
+    # Forgive me for the crimes I have committed here
     if vis is None:
-        cur.execute("""\
+        cur.execute(
+            """\
             select count(*)
             from general_series gs
             join general_image gi 
                 on gi.general_series_pk_id = gs.general_series_pk_id
-            where gs.project = %s
-        """, [collection_name])
+            where gs.project = %(project)s
+            and (%(site)s = "" or gs.site = %(site)s)
+        """,
+            {
+                "project": collection_name,
+                "site": site_name
+            },
+        )
 
     else:
-        cur.execute(f"""\
+        cur.execute(
+            f"""\
             select count(*)
             from general_series gs
             join general_image gi 
                 on gi.general_series_pk_id = gs.general_series_pk_id
-            where gs.project = %s
+            where gs.project = %(project)s
+            and (%(site)s = "" or gs.site = %(site)s)
             and gs.visibility in ({','.join(vis)})
-        """, [collection_name])
+        """,
+            {
+                "project": collection_name,
+                "site": site_name
+            },
+        )
 
-def execute_select_query(cur, collection_name, vis):
+
+def execute_select_query(cur, collection_name, vis, site_name):
     if vis is None:
-        cur.execute("""\
+        cur.execute(
+            """\
             select gi.dicom_file_uri, gi.md5_digest
             from general_series gs
             join general_image gi 
                 on gi.general_series_pk_id = gs.general_series_pk_id
-            where gs.project = %s
-        """, [collection_name])
+            where gs.project = %(project)s
+            and (%(site)s = "" or gs.site = %(site)s)
+        """,
+            {
+                "project": collection_name,
+                "site": site_name
+            },
+        )
     else:
-        cur.execute(f"""\
+        cur.execute(
+            f"""\
             select gi.dicom_file_uri, gi.md5_digest
             from general_series gs
             join general_image gi 
                 on gi.general_series_pk_id = gs.general_series_pk_id
-            where gs.project = %s
+            where gs.project = %(project)s
+            and (%(site)s = "" or gs.site = %(site)s)
             and gs.visibility in ({','.join(vis)})
-        """, [collection_name])
+        """,
+            {
+                "project": collection_name,
+                "site": site_name
+            },
+        )
 
-def main(background_id, activity_id, notify, collection_name, visibility):
+
+def main(background_id, activity_id, notify, collection_name, visibility, site_name):
     background = BackgroundProcess(background_id, notify, activity_id)
     background.daemonize()
 
     print(f"Beginning import of collection {collection_name} from Public.")
-    import_comment=f"Importing {collection_name} from public (in-place)"
+    import_comment = f"Importing {collection_name} from public (in-place)"
     import_event_id = create_import_event(import_comment)
     print(f"Import event id: {import_event_id}")
 
     with Database("public") as conn:
         cur = conn.cursor()
 
-
         vis_list = parse_visibility(visibility)
 
-        execute_count_query(cur, collection_name, vis_list)
+        execute_count_query(cur, collection_name, vis_list, site_name)
 
-        for count, in cur:
+        for (count,) in cur:
             total_files_to_import = count
 
-        execute_select_query(cur, collection_name, vis_list)
+
+        execute_select_query(cur, collection_name, vis_list, site_name)
 
         # collect all of the filenames into memory
         # NOTE: This is done because, for larger collections
@@ -179,26 +211,42 @@ def main(background_id, activity_id, notify, collection_name, visibility):
 
     if error_count > 0:
         print(f"Unfortunately there were {error_count} errors! See STDERR")
-    background.finish(f"Complete - imported {total_files_to_import - error_count} files")
+    background.finish(
+        f"Complete - imported {total_files_to_import - error_count} files"
+    )
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument(
-        'background_id',
-        help='background_subprocess_id, should be supplied by Posda'
+        "background_id", help="background_subprocess_id, should be supplied by Posda"
     )
-    parser.add_argument('activity_id', help='the activity you want to convert')
-    parser.add_argument('notify', help='the person to notify when complete')
-    parser.add_argument('collection_name', help='the collection to import')
-    parser.add_argument('visibility', help='''comma-seperated list of 
+    parser.add_argument("activity_id", help="the activity you want to convert")
+    parser.add_argument("notify", help="the person to notify when complete")
+    parser.add_argument("collection_name", help="the collection to import")
+    parser.add_argument(
+        "visibility",
+        help="""comma-seperated list of 
                         visibilities to include in the copy. If left blank,
-                        all files are copied.''')
+                        all files are copied.""",
+    )
+    parser.add_argument(
+        "site_name",
+        help="the site to include; leave blank for all",
+    )
 
     return parser.parse_args()
+
 
 if __name__ == "__main__":
     args = parse_args()
 
     printe("Running with args: ", args)
-    main(args.background_id, args.activity_id, args.notify, args.collection_name, args.visibility)
+    main(
+        args.background_id,
+        args.activity_id,
+        args.notify,
+        args.collection_name,
+        args.visibility,
+        args.site_name,
+    )
