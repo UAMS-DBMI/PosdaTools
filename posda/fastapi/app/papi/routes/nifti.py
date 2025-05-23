04a890dcfea9c4f381345025f37a92b4f88d4f06
Michael Rutherford <mwrutherford@uams.edu>
2025-05-23 09:32:03 -0500

    Added endpoint to retrieve Nifti VR files

--- posda/fastapi/app/papi/routes/nifti.py
+++ posda/fastapi/app/papi/routes/nifti.py
@@ -40,7 +40,8 @@ async def get_nifti_details(
     """    
 
     query = """
-        select fi.file_name, 
+        select fn.file_id,
+               fi.file_name, 
                fsr.root_path, 
                fl.rel_path,
                fn.is_zipped
@@ -101,131 +102,23 @@ async def set_status(
         raise HTTPException(detail="An unexpected error occurred", status_code=500)
 
 
+@router.get("/visualreview/{nifti_visual_review_instance_id}")
+async def get_for_visualreview(
+    visual_review_instance_id: int,
+    db: Database = Depends(),
+    current_user: User = logged_in_user
+):
+    """Return list of all Files in this Nifti VR
+    """
 
+    try:
+        records = await db.fetch("""\
+            select nifti_file_id 
+            from nifti_visual_review_files
+            where nifti_visual_review_instance_id = $1
+        """, [visual_review_instance_id])
 
-# @router.get("/review/{vr_id}")
-# async def review(vr_id: int, db: Database = Depends()):
-#     query = """\
-#     select distinct
-#         fi.file_name,
-#         nvrs.good_status
-#     from
-#         nifti_visual_review_files nvrf
-#         left join file_import fi 
-#         on fi.file_id = nvrf.path_file_id
-#         left join nifti_visual_review_status nvrs 
-#         on nvrf.path_file_id = nvrs.path_file_id
-#     where
-#         nvrf.nifti_visual_review_instance_id = $1
-#       """
-#     return await db.fetch(query, [vr_id])
+        return [x[0] for x in records]
 
-
-
-
-
-
-# @router.get("/mapping/{file_id}")
-# async def get_mapping(file_id: int, db: Database = Depends()):
-#     query = """\
-#          select
-#             patient_id
-#         from
-#             pathology_patient_mapping a
-#         where
-#             a.file_id = $1
-#              """
-#     return await db.fetch(query, [file_id])
-
-# @router.get("/image_desc/{file_id}")
-# async def get_image_desc(file_id: int, db: Database = Depends()):
-#     query = """\
-#          select
-#             image_desc
-#         from
-#             pathology_image_desc a
-#         where
-#             a.file_id = $1
-#              """
-#     return await db.fetch(query, [file_id])
-
-
-# @router.get("/getcount")
-# def get_current_count():
-#     return len(images)
-
-# @router.get("/find_files/{activity_id}")
-# async def find_files(activity_id: int, db: Database = Depends()):
-#     query = """\
-#         select f.file_id from file f join activity_timepoint_file atf  on f.file_id = atf.file_id
-#             where atf.activity_timepoint_id in (
-#                 select
-#                     max(activity_timepoint_id) as activity_timepoint_id
-#                 from
-#                     activity_timepoint
-#                 where
-#                     activity_id = $1
-#               );
-#         """
-#     return await db.fetch(query, [activity_id])
-
-# @router.get("/find_edits/{file_id}")
-# async def find_edits(file_id: int, db: Database = Depends()):
-#     query = """\
-#      select pathology_edit_queue_id, edit_type, edit_details
-#             from pathology_edit_queue
-#              where
-#                  file_id = $1
-#                  and status = 'waiting'
-#     """
-#     return await db.fetch(query, [file_id])
-
-# @router.get("/find_relpath/{file_id}")
-# async def find_relpath(file_id: int, db: Database = Depends()):
-#     query = """\
-#       select root_path, rel_path
-#       from file_location f
-#       natural join file_storage_root fsr
-#       where f.file_id = $1
-#      """
-#     return await db.fetch(query, [file_id])
-
-# @router.patch("/completeEdit/{edit_id}")
-# async def completeEdit(edit_id: int, db: Database = Depends()):
-#         record = await db.fetch("""\
-#         update pathology_edit_queue set status = 'complete' where pathology_edit_queue_id = $1 returning pathology_edit_queue_id;
-#         """, [edit_id])
-
-#         if len(record) < 1:
-#             raise HTTPException(detail="Complete Edit: Error updating edit status", status_code=422)
-
-#         return {
-#            'status': 'success',
-#         }
-
-# @router.put("/create_path_activity_timepoint/{activity_id}/{user}")
-# async def create_path_activity_timepoint(activity_id: int, user: str, db: Database = Depends()):
-#        query = """\
-#             insert into activity_timepoint(
-#                 activity_id,
-#                 when_created,
-#                 who_created,
-#                 comment,
-#                 creating_user
-#             ) values (
-#                 $1, now(), $2, 'Post Edit Pathology TP', $2)
-#             returning activity_timepoint_id;
-#             """
-#        return await db.fetch(query, [activity_id, user])
-
-# @router.put("/add_file_to_path_activity_timepoint/{atf_id}/{file_id}")
-# async def add_file_to_path_activity_timepoint(atf_id: int, file_id: int, db: Database = Depends()):
-#          query = """\
-#                     insert into activity_timepoint_file(
-#                          activity_timepoint_id,
-#                          file_id
-#                         ) values (
-#                           $1, $2)
-#                     returning file_id;
-#                     """
-#          return await db.fetch(query, [atf_id, file_id])
+    except:
+        pass
