843b1f9c6872945d20a6da26764b35e039598f73
sutecht <sutecht@uams.edu>
2025-07-24 11:33:28 -0700

    migration file cleanup

--- database/migrations/posda_files/alter_path_pm_1010.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE public.pathology_patient_mapping ADD study_name text NULL;
-ALTER TABLE public.pathology_patient_mapping ADD image_id text NULL;
-ALTER TABLE public.pathology_patient_mapping ADD clinical_trial_subject_id text NULL;
