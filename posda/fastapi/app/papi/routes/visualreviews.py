659b09acea8b7fdec41fafea72874a12c8c40417
Quasar Jarosz <quasar@uams.edu>
2025-10-23 16:58:51 -0500

    Add new visualreview endpoints: values and filter
    These will allow retrieving a filtered list of IECs in a given
    Visual Review instance

--- posda/fastapi/app/papi/routes/visualreviews.py
+++ posda/fastapi/app/papi/routes/visualreviews.py
@@ -53,3 +53,143 @@ async def get_vr_iecs(vr: int, db: Database = Depends()):
     """
 
     return [x[0] for x in await db.fetch(query, [vr])]
+
+
+@router.get("/{vr}/values")
+async def get_vr_extra_details(vr: int, db: Database = Depends()):
+    """
+    Get list of valid values for use in filtering, for the given VR.
+
+    Returns the valid values for: review_status, processing_status, and 
+    dicom_file_type
+    """
+
+    dicom_file_type_query = """\
+        with iecs as (
+            select image_equivalence_class_id, review_status, processing_status
+            from image_equivalence_class
+            where visual_review_instance_id = $1
+        )
+        select
+            (
+                select dicom_file_type
+                from image_equivalence_class_input_image
+                natural join dicom_file
+                where image_equivalence_class_input_image.image_equivalence_class_id = iecs.image_equivalence_class_id
+                limit 1
+            ),
+            count(*)
+        from iecs
+        group by 1
+    """
+
+    review_status_query = """\
+        with iecs as (
+            select image_equivalence_class_id, review_status, processing_status
+            from image_equivalence_class
+            where visual_review_instance_id = $1
+        )
+        select
+            review_status,
+            count(*) as count
+        from iecs
+        group by 1
+    """
+
+    processing_status_query = """\
+        with iecs as (
+            select image_equivalence_class_id, review_status, processing_status
+            from image_equivalence_class
+            where visual_review_instance_id = $1
+        )
+        select
+            processing_status,
+            count(*) as count
+        from iecs
+        group by 1
+    """
+
+
+    dicom_file_types = await db.fetch(dicom_file_type_query, [vr])
+    review_statuses = await db.fetch(review_status_query, [vr])
+    processing_statuses = await db.fetch(processing_status_query, [vr])
+
+    return {
+        'dicom_file_types': dicom_file_types,
+        'review_statuses': review_statuses,
+        'processing_statuses': processing_statuses,
+    }
+
+
+class VRFilterParameters(BaseModel):
+    dicom_file_type: Optional[str] = '*'
+    processing_status: Optional[str] = '*'
+    review_status: Optional[str] = '*'
+
+@router.post("/{vr}/filter")
+async def get_vr_filtered(vr: int, params: VRFilterParameters, db: Database = Depends()):
+    """
+    Return a filtered list of IECs in this VR. Use * for wildcard.
+    """
+    query = """\
+        with iecs as (
+            select
+            image_equivalence_class_id,
+            review_status,
+            processing_status,
+                (
+                    /*
+                            All files in an IEC must always be of the same dicom_file_type,
+                            so we can just select the first one here for speed
+                    */
+                    select dicom_file_type
+                    from image_equivalence_class_input_image
+                    natural join dicom_file
+                    where image_equivalence_class_input_image.image_equivalence_class_id =
+                            image_equivalence_class.image_equivalence_class_id
+                    limit 1
+                ) dicom_file_type
+            from image_equivalence_class
+            where visual_review_instance_id = $1
+        )
+
+        select image_equivalence_class_id
+        from iecs
+        where 1 = 1
+
+    """
+
+    bind_vars = [vr]
+    bind_count = 1
+
+    if params.dicom_file_type != '*':
+        bind_count += 1
+        query += f"and dicom_file_type = ${bind_count}\n"
+        bind_vars.append(params.dicom_file_type)
+
+    if params.processing_status != '*':
+        bind_count += 1
+        query += f"and processing_status = ${bind_count}\n"
+        bind_vars.append(params.processing_status)
+
+    if params.review_status != '*':
+        bind_count += 1
+        query += f"and review_status = ${bind_count}\n"
+        bind_vars.append(params.review_status)
+
+    results = await db.fetch(query, bind_vars)
+
+    # If no results at all are returned, do a second check to see if 
+    # the reason is because the VR was invalid.
+    if len(results) == 0:
+        test_results = await db.fetch("""\
+            select visual_review_instance_id
+            from visual_review_instance
+            where visual_review_instance_id = $1
+        """, (vr,))
+
+        if len(test_results) == 0:
+            raise HTTPException(detail="invalid visual review id", status_code=404)
+
+    # flatten the results (we just want a list of iecs returned)
+    return [x[0] for x in results]
