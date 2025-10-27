51208590b53c7ec63ebda501f619959c727cc793
Haydex <hayddex@gmail.com>
2025-10-27 12:37:44 -0500

    Kohlrabi Next.js project setup.

---
+++ web/Kohlrabi/next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  turbopack: {
+    root: "/Users/hayderphd/Library/CloudStorage/OneDrive-UniversityofArkansasforMedicalSciences/UAMSONEDRIVE/oneposda/oneposda/web/kohlrabi",
+  },
+};
+
+export default nextConfig;
