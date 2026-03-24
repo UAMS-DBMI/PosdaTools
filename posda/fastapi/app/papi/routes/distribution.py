ba7be2fa1f91c161b9941a1886b1d8ce97950bbc
sutecht <sutecht@uams.edu>
2026-03-24 12:27:53 -0700

    updated to new naming

--- posda/fastapi/app/papi/routes/distribution.py
+++ posda/fastapi/app/papi/routes/distribution.py
@@ -9,93 +9,94 @@ router = APIRouter(
     dependencies=[logged_in_user]
 )
 
-# Collections
-
-#Note, the datastructure diagram did not include some suggested data
-# such as description and creation date
-@router.get("/collections/{collection_id}")
-async def get_collections_by_id(collection_id: int, db: Database = Depends()):
+# datasets
+@router.get("/datasets/{dataset_id}")
+async def get_datasets_by_id(dataset_id: int, db: Database = Depends()):
     query = """\
         select
-            c.collection_id,
-            c.collection_code,
-            c.collection_name,
+            c.dataset_id,
+            c.dataset_doi,
+            c.dataset_title,
+            c.dataset_short_title,
+            c.dataset_name,
+            c.when_created,
             latest_release.latest_release_id,
-            my_datasets.dataset_count
+            my_recordsets.recordset_count
         from
-            collection c
+            dataset c
             natural join
                 (select
-                    collection_id,
-                    max(collection_release_id) as latest_release_id
+                    dataset_id,
+                    max(dataset_release_id) as latest_release_id
                     from
-                        collection_release
+                        dataset_release
                     where
-                        collection_id = $1
+                        dataset_id = $1
                     group by
-                        collection_id
+                        dataset_id
                 ) latest_release
             natural join
                 (select
-                    count(dataset_id) as dataset_count
+                    count(recordset_id) as recordset_count
                  from
-                    collection natural join dataset
+                    dataset natural join recordset
                  where
-                    collection_id = $1) as my_datasets
+                    dataset_id = $1) as my_recordsets
         where
-            c.collection_id = $1
+            c.dataset_id = $1
         """
-    return await db.fetch(query, [collection_id])
+    return await db.fetch(query, [dataset_id])
 
-@router.get("/collections/{collection_id}/datasets")
-async def get_datasets_by_collection(collection_id: int, db: Database = Depends()):
+@router.get("/datasets/{dataset_id}/recordsets")
+async def get_recordsets_by_dataset(dataset_id: int, db: Database = Depends()):
     query = """\
         select
-            ds.dataset_id,
-            ds.dataset_doi,
-            ds.dataset_title,
-            ds.dataset_type,
-            dsl.dataset_license_label,
-            dsl.dataset_license_url
+            ds.recordset_id,
+            ds.recordset_doi,
+            ds.recordset_title,
+            ds.recordset_type,
+            dsl.license_label,
+            dsl.license_url,
+            dsl.is_public_access
         from
-            collection natural join dataset ds
-            natural join dataset_license dsl
+            dataset natural join recordset ds
+            natural join recordset_license dsl
         where
-            collection_id = $1
+            dataset_id = $1
         """
-    return await db.fetch(query, [collection_id])
+    return await db.fetch(query, [dataset_id])
 
-@router.get("/collections/{collection_id}/releases")
-async def get_releases_by_collection(collection_id: int, db: Database = Depends()):
+@router.get("/datasets/{dataset_id}/releases")
+async def get_releases_by_dataset(dataset_id: int, db: Database = Depends()):
     query = """\
         select
-            cr.collection_release_id,
-            cr.collection_release_num,
-            cr.collection_release_date,
-            crds.dataset_release_id
+            cr.dataset_release_id,
+            cr.release_number,
+            cr.release_date,
+            crds.recordset_release_id
         from
-            collection c
-            natural join collection_release cr
-            natural join collection_release_dataset crds
+            dataset c
+            natural join dataset_release cr
+            natural join dataset_release_recordset crds
         where
-            c.collection_id = $1;
+            c.dataset_id = $1;
         """
-    return await db.fetch(query, [collection_id])
+    return await db.fetch(query, [dataset_id])
 
-#Note added dataset_release_id as input
-#This structure implies the dataset_release record was inserted prior to this call
+#Note added recordset_release_id as input
+#This structure implies the recordset_release record was inserted prior to this call
 #It also assumes that the PK and release number are auto-incrementing
-@router.post("/collections/{collection_id}/releases/{dataset_release_id}")
-async def add_release_to_collection(collection_id: int, dataset_release_id: int, db: Database = Depends()):
+@router.post("/datasets/{dataset_id}/releases/{recordset_release_id}")
+async def add_release_to_dataset(dataset_id: int, recordset_release_id: int, db: Database = Depends()):
     record = await db.fetch("""\
         with new_release as (
-            insert into collection_release (collection_id, collection_release_date)
+            insert into dataset_release (dataset_id, release_date)
             values ($1, now())
-            returning collection_release_id
+            returning dataset_release_id
         )
-        insert into collection_release_dataset (collection_release_id, dataset_release_id)
-        select collection_release_id, $2 from new_release
-    """, [collection_id,dataset_release_id])
+        insert into dataset_release_recordset (dataset_release_id, recordset_release_id)
+        select dataset_release_id, $2 from new_release
+    """, [dataset_id,recordset_release_id])
     print(record)
     if not record:
         raise HTTPException(detail="Error updating edit status", status_code=422)
@@ -103,18 +104,18 @@ async def add_release_to_collection(collection_id: int, dataset_release_id: int,
         'status': 'success',
     }
 
-@router.get("/collection-releases/{collection_release_id}")
-async def get_collection_release_details_by_id(collection_release_id: int, db: Database = Depends()):
+@router.get("/dataset-releases/{dataset_release_id}")
+async def get_dataset_release_details_by_id(dataset_release_id: int, db: Database = Depends()):
     query = """\
         select
-            cr.collection_release_id,
-            cr.collection_release_num,
-            cr.collection_release_date,
-            crds.dataset_release_id
+            cr.dataset_release_id,
+            cr.release_number,
+            cr.release_date,
+            crds.recordset_release_id
         from
-            collection_release cr
-            natural join collection_release_dataset crds
+            dataset_release cr
+            natural join dataset_release_recordset crds
         where
-            cr.collection_release_id = $1;
+            cr.dataset_release_id = $1;
         """
-    return await db.fetch(query, [collection_release_id])
+    return await db.fetch(query, [dataset_release_id])
